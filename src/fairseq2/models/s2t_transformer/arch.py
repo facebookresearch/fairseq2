1e0b9c315c62245bba7178cd21dee92abc1659dc
Can Balioglu <cbalioglu@users.noreply.github.com>
2023-02-25 14:56:56 -0500

    S2T transformer

---
+++ src/fairseq2/models/s2t_transformer/arch.py
@@ -0,0 +1,314 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import math
+from typing import Optional, Tuple
+
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Module
+
+from fairseq2.models.s2t_transformer.subsampler import FbankSubsampler
+from fairseq2.models.transformer.arch import TransformerTokenFrontend
+from fairseq2.nn.incremental_state import IncrementalStateBag
+from fairseq2.nn.positional_embedding import PositionalEmbedding
+from fairseq2.nn.projection import Linear, Projection
+from fairseq2.nn.transformer.decoder import TransformerDecoder
+from fairseq2.nn.transformer.encoder import TransformerEncoder
+from fairseq2.nn.utils.mask import to_padding_mask
+
+
+class TransformerFbankFrontend(Module):
+    """Represents a Transformer front-end as described in Section 2.1 of
+    :cite:t:`DBLP:journals/corr/abs-1911-08460`."""
+
+    subsampler: FbankSubsampler
+    scale: float
+    pos_embed: Optional[PositionalEmbedding]
+    proj: Optional[Projection]
+    dropout_p: float
+
+    def __init__(
+        self,
+        subsampler: FbankSubsampler,
+        pos_embed: Optional[PositionalEmbedding],
+        apply_projection: bool = False,
+        dropout_p: float = 0.1,
+        device=None,
+        dtype=None,
+    ) -> None:
+        """
+        :param subsampler:
+            The log-mel filterbank subsampler.
+        :param pos_embed:
+            The positional embedding.
+        :param apply_projection:
+            If ``True``, applies a projection to embeddings before dropout
+            as described in Section 2 of
+            :cite:t:`https://doi.org/10.48550/arxiv.2005.08100`.
+        :param dropout_p:
+            The dropout probability on embeddings.
+        """
+        super().__init__()
+
+        embedding_dim = subsampler.embedding_dim
+
+        self.subsampler = subsampler
+
+        self.scale = math.sqrt(embedding_dim)
+
+        if pos_embed is not None:
+            if pos_embed.embedding_dim != embedding_dim:
+                raise ValueError(
+                    f"`embedding_dim` of `pos_embed` ({pos_embed.embedding_dim}) does not match `embedding_dim` of `subsampler` ({embedding_dim})."
+                )
+
+            self.pos_embed = pos_embed
+        else:
+            self.register_module("pos_embed", None)
+
+        if apply_projection:
+            self.proj = Linear(embedding_dim, embedding_dim, device=device, dtype=dtype)
+        else:
+            self.register_module("proj", None)
+
+        self.dropout_p = dropout_p
+
+    def forward(
+        self,
+        fbanks: Tensor,
+        num_frames: Tensor,
+        state_bag: Optional[IncrementalStateBag] = None,
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        """
+        :param fbanks:
+            The log-mel filterbanks to subsample. *Shape:* :math:`(N,F,C)`, or
+            :math:`(F,C)` when unbatched, where :math:`N` is the batch size,
+            :math:`F` is the number of frames, and :math:`C` is the number of
+            channels.
+        :param num_frames:
+            An array where each entry defines the number of frames of the
+            filterbank at the same index in ``fbanks``. *Shape:* :math:`(N)`,
+            :math:`(N,1)`, or :math:`()` when unbatched, where :math:`N` is the
+            batch size.
+        :param state_bag:
+            The state bag to use during an incremental evaluation.
+
+        :returns:
+            - The audio embeddings to pass to the encoder or decoder. *Shape:*
+              :math:`(N,S,M)`, or :math:`(S,M)` when unbatched, where :math:`N`
+              is the batch size, :math:`S` is the sequence length, and :math:`M`
+              is the model size.
+            - The boolean padding mask indicating which key positions to ignore
+              for the purpose of self attention. *Shape:* :math:`(N,S)`, or
+              :math:`(S)` when unbatched, where :math:`N` is the batch size and
+              :math:`S` is the sequence length.
+
+        .. note::
+            For a boolean padding mask, a ``True`` indicates that the
+            corresponding key position is not allowed to attend.
+        """
+        embeds, seq_lens = self.subsampler(fbanks, num_frames)
+
+        embeds = embeds * self.scale
+
+        if self.pos_embed is not None:
+            embeds = self.pos_embed(embeds, state_bag)
+
+        if self.proj is not None:
+            embeds = self.proj(embeds)
+
+        if self.dropout_p > 0.0:
+            embeds = F.dropout(embeds, self.dropout_p, self.training)
+
+        padding_mask = to_padding_mask(
+            seq_lens, max_seq_len=embeds.size(0 if embeds.dim() == 2 else 1)
+        )
+
+        return embeds, padding_mask
+
+    def extra_repr(self) -> str:
+        """:meta private:"""
+        return f"scale={self.scale}, dropout_p={self.dropout_p}"
+
+
+class S2TTransformer(Module):
+    """Represents a speech-to-text Transformer model as described in Section 2.1
+    of :cite:t:`DBLP:journals/corr/abs-1911-08460`.
+
+    A learned filterbank subsampler is used as the front-end of the Transformer
+    encoder to project inputs into embeddings and to reduce sequence lengths for
+    computational efficiency.
+    """
+
+    model_dim: int
+    """The dimensionality of the model (i.e. inputs and outputs)."""
+
+    encoder_frontend: TransformerFbankFrontend
+    encoder: TransformerEncoder
+    decoder_frontend: TransformerTokenFrontend
+    decoder: TransformerDecoder
+    score_proj: Projection
+
+    def __init__(
+        self,
+        encoder_frontend: TransformerFbankFrontend,
+        encoder: TransformerEncoder,
+        decoder_frontend: TransformerTokenFrontend,
+        decoder: TransformerDecoder,
+        score_proj: Projection,
+    ) -> None:
+        """
+        :param encoder_frontend:
+            The encoder front-end.
+        :param encoder:
+            The encoder.
+        :param decoder_frontend:
+            The decoder front-end.
+        :param decoder:
+            The decoder.
+        :param score_proj:
+            The projection to apply to outputs of the decoder.
+        """
+        model_dim = encoder.model_dim
+
+        if decoder.model_dim != model_dim:
+            raise ValueError(
+                f"`model_dim` of `encoder` ({encoder.model_dim}) does not match `model_dim` of `decoder` ({decoder.model_dim})."
+            )
+
+        if encoder_frontend.subsampler.embedding_dim != model_dim:
+            raise ValueError(
+                f"`embedding_dim` of `encoder_frontend.subsampler` ({encoder_frontend.subsampler.embedding_dim}) does not match `model_dim` of `encoder` ({model_dim})."
+            )
+
+        if decoder_frontend.embed.embedding_dim != model_dim:
+            raise ValueError(
+                f"`embedding_dim` of `decoder_frontend.embed` ({decoder_frontend.embed.embedding_dim}) does not match `model_dim` of `decoder` ({model_dim})."
+            )
+
+        super().__init__()
+
+        self.model_dim = model_dim
+
+        self.encoder_frontend = encoder_frontend
+        self.encoder = encoder
+
+        self.decoder_frontend = decoder_frontend
+        self.decoder = decoder
+
+        self.score_proj = score_proj
+
+    def encode(
+        self, fbanks: Tensor, num_frames: Tensor
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        """
+        Encodes the specified source log-mel filterbanks.
+
+        :param fbanks:
+            The log-mel filterbanks to encode. *Shape:* :math:`(N,F,C)`, or
+            :math:`(F,C)` when unbatched, where :math:`N` is the batch size,
+            :math:`F` is the number of frames, and :math:`C` is the number of
+            channels.
+        :param num_frames:
+            An array where each entry defines the number of frames of the
+            filterbank at the same index in ``fbanks``. *Shape:* :math:`(N)`,
+            :math:`(N,1)`, or :math:`()` when unbatched, where :math:`N` is the
+            batch size.
+
+        :returns:
+            - The encoded output. *Shape:* :math:`(N,S,M)`, or :math:`(S,M)`
+              when unbatched, where :math:`N` is the batch size, :math:`S` is
+              the sequence length, and :math:`M` is the model size.
+            - The boolean padding mask indicating which key positions to ignore
+              for the purpose of encoder-decoder attention. *Shape:*
+              :math:`(N,S)`, or :math:`(S)` when unbatched, where :math:`N` is
+              the batch size and :math:`S` is the sequence length.
+
+        .. note::
+            For a boolean padding mask, a ``True`` indicates that the
+            corresponding key position is not allowed to attend.
+        """
+        embeds, padding_mask = self.encoder_frontend(fbanks, num_frames)
+
+        x = self.encoder(embeds)
+
+        return x, padding_mask
+
+    def decode_and_score(
+        self,
+        token_indices: Tensor,
+        enc_out: Tensor,
+        enc_padding_mask: Optional[Tensor] = None,
+        state_bag: Optional[IncrementalStateBag] = None,
+    ) -> Tensor:
+        """
+        Decodes the specified target token indices.
+
+        :param token_indices:
+            The token indices to decode. *Shape:* :math:`(N,S)`, or :math:`(S)`
+            when unbatched, where :math:`N` is the batch size and :math:`S` is
+            the sequence length.
+        :param enc_out:
+            The encoder output for the encoder-decoder attention. *Shape:*
+            :math:`(N,S_{src},M)`, or :math:`(S_{src},M)` when unbatched, where
+            :math:`N` is the batch size, :math:`S_{src}` is the source sequence
+            length, and :math:`M` is the model size.
+        :param enc_padding_mask:
+            The boolean or float padding mask indicating which key positions to
+            ignore for the purpose of encoder-decoder attention. *Shape:*
+            :math:`(N,S_{src})`, or :math:`(S_{src})` when unbatched, where
+            :math:`N` is the batch size and :math:`S_{src}` is the source
+            sequence length.
+        :param state_bag:
+            The state bag to use during an incremental evaluation.
+
+        :returns:
+            The output of :attr:`score_proj`. A softmax function should be
+            applied to the produced scores to obtain the next-token
+            probabilities. *Shape:* :math:`(N,S,D)`, or :math:`(S,D)` when
+            unbatched, where :math:`N` is the batch size, :math:`S` is the
+            sequence length, and :math:`D` is the size of the output embedding
+            dictionary.
+        """
+        embeds, padding_mask = self.decoder_frontend(token_indices, state_bag)
+
+        x = self.decoder(embeds, padding_mask, enc_out, enc_padding_mask, state_bag)
+
+        x = self.score_proj(x)
+
+        return x  # type: ignore[no-any-return]
+
+    def forward(
+        self, fbanks: Tensor, num_frames: Tensor, tgt_token_indices: Tensor
+    ) -> Tensor:
+        """
+        :param fbanks:
+            The source log-mel filterbanks to encode. *Shape:* :math:`(N,F,C)`,
+            or :math:`(F,C)` when unbatched, where :math:`N` is the batch size,
+            :math:`F` is the number of frames, and :math:`C` is the number of
+            channels.
+        :param num_frames:
+            An array where each entry defines the number of frames of the source
+            filterbank at the same index in ``fbanks``. *Shape:* :math:`(N)`,
+            :math:`(N,1)`, or :math:`()` when unbatched, where :math:`N` is the
+            batch size.
+        :param tgt_token_indices:
+            The target token indices to decode. *Shape:* :math:`(N,S_{tgt})`, or
+            :math:`(S_{tgt})` when unbatched, where :math:`N` is the batch size
+            and :math:`S_{tgt}` is the target sequence length.
+
+        :returns:
+            The output of :attr:`score_proj`. A softmax function should be
+            applied to the produced scores to obtain the next-token
+            probabilities. *Shape:* :math:`(N,S_{tgt},D)`, or
+            :math:`(S_{tgt},D)` when unbatched, where :math:`N` is the batch
+            size, :math:`S_{tgt}` is the target sequence length, and :math:`D`
+            is the size of the output embedding dictionary.
+        """
+        enc_out, enc_padding_mask = self.encode(fbanks, num_frames)
+
+        return self.decode_and_score(tgt_token_indices, enc_out, enc_padding_mask)
---
+++ src/fairseq2/models/s2t_transformer/subsampler.py
@@ -0,0 +1,139 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from abc import ABC, abstractmethod
+from typing import Optional, Sequence, Tuple, final
+
+import torch.nn.functional as F
+from overrides import final as finaloverride
+from torch import Tensor
+from torch.nn import Conv1d, Module, ModuleList
+
+
+class FbankSubsampler(Module, ABC):
+    """Subsamples log-mel filterbanks and embeds them in a latent space for use
+    in sequence encoding and decoding."""
+
+    embedding_dim: int
+    """The dimensionality of returned embeddings."""
+
+    def __init__(self, embedding_dim: int) -> None:
+        super().__init__()
+
+        self.embedding_dim = embedding_dim
+
+    @abstractmethod
+    def forward(
+        self, fbanks: Tensor, num_frames: Tensor
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        """
+        :param fbanks:
+            The log-mel filterbanks to subsample. *Shape:* :math:`(N,F,C)`, or
+            :math:`(F,C)` when unbatched, where :math:`N` is the batch size,
+            :math:`F` is the number of frames, and :math:`C` is the number of
+            channels.
+        :param num_frames:
+            An array where each entry defines the number of frames of the
+            filterbank at the same index in ``fbanks``. *Shape:* :math:`(N)`,
+            :math:`(N,1)`, or :math:`()` when unbatched, where :math:`N` is the
+            batch size.
+
+        :returns:
+            - The audio embeddings to pass to the encoder or decoder. *Shape:*
+              :math:`(N,S,E)`, or :math:`(S,E)` when unbatched, where :math:`N`
+              is the batch size, :math:`S` is the sequence length, and :math:`E`
+              is the embedding size.
+            - The sequence lengths corresponding to the returned audio
+              embeddings. *Shape:* :math:`(N)`, or :math:`()` when unbatched,
+              where :math:`N` is the batch size.
+        """
+
+    def extra_repr(self) -> str:
+        """:meta private:"""
+        return f"embedding_dim={self.embedding_dim}"
+
+
+@final
+class Conv1dFbankSubsampler(FbankSubsampler):
+    """Represents a 1D convolutional subsampler as described in Section 2.1 of
+    :cite:t:`DBLP:journals/corr/abs-1911-08460`."""
+
+    convs: ModuleList
+
+    def __init__(
+        self,
+        num_channels: int,
+        inner_dim: int,
+        embedding_dim: int,
+        kernel_sizes: Optional[Sequence[int]] = None,
+        device=None,
+        dtype=None,
+    ) -> None:
+        """
+        :param num_channels:
+            The number of channels of input log-mel filterbanks.
+        :param inner_dim:
+            The dimensionality of the intermediate 1D convolution layers.
+        :param embedding_dim:
+            The dimensionality of returned embeddings.
+        :param kernel_sizes:
+            The kernel size of each 1D convolutional layer.
+        """
+        super().__init__(embedding_dim)
+
+        if kernel_sizes is None:
+            kernel_sizes = [3, 3]
+
+        if not kernel_sizes:
+            raise ValueError("`kernel_sizes` must contain at least one element.")
+
+        last_layer = len(kernel_sizes) - 1
+
+        convs = [
+            Conv1d(
+                num_channels if i == 0 else inner_dim // 2,
+                inner_dim if i < last_layer else embedding_dim * 2,
+                kernel_size,
+                stride=2,
+                padding=kernel_size // 2,
+                device=device,
+                dtype=dtype,
+            )
+            for i, kernel_size in enumerate(kernel_sizes)
+        ]
+
+        self.convs = ModuleList(convs)
+
+    @finaloverride
+    def forward(
+        self, fbanks: Tensor, num_frames: Tensor
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        # Apply the convolution along the temporal dimension (i.e. along the
+        # sequence).
+        # (N, F, C) -> (N, C, F)
+        x = fbanks.transpose(1, 2)
+
+        for conv in self.convs:
+            x = conv(x)
+
+            x = F.glu(x, dim=1)
+
+        # (N, E, F), -> (N, F, E)
+        x = x.transpose(1, 2)
+
+        # Since we contracted the temporal dimension, we should re-compute the
+        # sequence lengths.
+        seq_lens = self._compute_seq_lens(num_frames)
+
+        return x, seq_lens
+
+    def _compute_seq_lens(self, num_frames: Tensor) -> Tensor:
+        seq_lens = num_frames.clone()
+
+        for _ in range(len(self.convs)):
+            seq_lens = (((seq_lens - 1) / 2.0) + 1.0).floor().type(seq_lens.dtype)
+
+        return seq_lens
