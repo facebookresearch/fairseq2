c38c7228595494df0694fe5f9db163d7fc038c7a
Can Balioglu <cbalioglu@users.noreply.github.com>
2023-06-24 18:24:46 -0400

    Simplify shared library loading

--- src/fairseq2/__init__.py
+++ src/fairseq2/__init__.py
@@ -4,54 +4,10 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import sys
-from ctypes import CDLL, RTLD_GLOBAL
 from pathlib import Path
 from typing import Optional, Tuple
 
 __version__ = "0.1.0.dev0"
-
-# We import `torch` to ensure that libtorch.so is loaded into the process before
-# our extension module.
-import torch  # noqa: F401
-
-# Holds the handle to the TBB shared library.
-_tbb: Optional[CDLL] = None
-
-
-def _load_tbb() -> None:
-    # TODO: Do not hard-code the so name.
-    if sys.platform == "darwin":
-        dso_name = "libtbb.12.dylib"
-    else:
-        dso_name = "libtbb.so.12"
-
-    global _tbb
-
-    # If the system already provides TBB, skip the rest. The dynamic linker will
-    # resolve it later. Do not use ctypes' find_library here since it hangs when
-    # run under ThreadSanitizer.
-    try:
-        _tbb = CDLL(dso_name, mode=RTLD_GLOBAL)
-    except OSError:
-        pass
-    else:
-        return
-
-    # Otherwise, load it from the tbb PyPI package if installed.
-    lib_path = Path(sys.executable).parent.parent.joinpath("lib", dso_name)
-
-    try:
-        _tbb = CDLL(str(lib_path), mode=RTLD_GLOBAL)
-    except OSError:
-        raise RuntimeError(
-            "Intel oneTBB is not found! Check your fairseq2 installation!"
-        )
-
-
-# We load TBB using our own lookup logic since it might be located in the
-# Python virtual environment.
-_load_tbb()
 
 from fairseq2 import C  # type: ignore[attr-defined]
 
