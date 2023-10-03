b0d8e549ce5b485bc97f4edf43d2ee51759c6fd8
Naji El Hachem <najielhachem@meta.com>
2023-10-03 10:11:27 +0200

    Fix circular data source stop_at_shortest flag

--- fairseq2n/src/fairseq2n/data/circular_data_source.cc
+++ fairseq2n/src/fairseq2n/data/circular_data_source.cc
@@ -18,6 +18,9 @@ circular_data_source::circular_data_source(std::vector<data_pipeline> &&pipeline
 std::optional<data>
 circular_data_source::next()
 {
+    if (stop_at_shortest_) // with this flag on, the operator is a simple iterator
+        return pipelines_[next_index_gen_()].next();
+
     // One or more data pipelines might be empty, so we have to keep looping
     std::optional<data> output{};
     while (!output && !eod()) {
@@ -36,38 +39,39 @@ circular_data_source::next()
 void
 circular_data_source::reset()
 {
-    buffer_.assign(pipelines_.size(), std::nullopt);
-
-    is_epoch_done_.assign(pipelines_.size(), false);
-
-    is_eod_ = false;
-
     for (data_pipeline &pipeline : pipelines_)
         pipeline.reset();
+
+    if (!stop_at_shortest_) {
+        buffer_.assign(pipelines_.size(), std::nullopt);
+        is_epoch_done_.assign(pipelines_.size(), false);
+        is_eod_ = false;
+    }
 }
 
 void
 circular_data_source::record_position(tape &t) const
 {
-    t.record(buffer_);
-
-    t.record(is_epoch_done_);
-
     for (const data_pipeline &pipeline : pipelines_)
         pipeline.record_position(t);
+
+    if (!stop_at_shortest_) {
+        t.record(buffer_);
+        t.record(is_epoch_done_);
+    }
 }
 
 void
 circular_data_source::reload_position(tape &t)
 {
-    buffer_ = t.read<std::vector<std::optional<data>>>();
-
-    is_epoch_done_ = t.read<std::vector<bool>>();
-
-    is_eod_ = false;
-
     for (data_pipeline &pipeline : pipelines_)
         pipeline.reload_position(t);
+
+    if (!stop_at_shortest_) {
+        buffer_ = t.read<std::vector<std::optional<data>>>();
+        is_epoch_done_ = t.read<std::vector<bool>>();
+        is_eod_ = false;
+    }
 }
 
 std::optional<data>
@@ -78,8 +82,6 @@ circular_data_source::next_in_pipeline(std::size_t pipeline_idx)
     std::optional<data> maybe_example = pipeline.next();
     if (!maybe_example) {
         is_epoch_done_[pipeline_idx] = true;
-        if (stop_at_shortest_) // we flag eod here for effeciency
-            is_eod_ = true;
 
         pipeline.reset();
 
