0f5a8cf378360e0c19e2b649617cee18dffb63a4
Alisha <alisha831maddy@gmail.com>
2023-10-09 15:36:02 -0700

    image decoder header file and constructor

---
+++ fairseq2n/src/fairseq2n/data/image/image_decoder.cc
@@ -0,0 +1,39 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "fairseq2n/data/image/image_decoder.h"
+
+#include <cstdint>
+#include <exception>
+#include <stdexcept>
+
+#include <ATen/Functions.h>
+#include <ATen/Tensor.h>
+
+#include "fairseq2n/exception.h"
+#include "fairseq2n/float.h"
+#include "fairseq2n/fmt.h"
+#include "fairseq2n/memory.h"
+#include "fairseq2n/span.h"
+#include "fairseq2n/data/audio/detail/sndfile.h"
+#include "fairseq2n/data/detail/tensor_helpers.h"
+#include "fairseq2n/detail/exception.h"
+
+using namespace fairseq2n::detail;
+
+namespace fairseq2n {
+    
+image_decoder::image_decoder(image_decoder_options opts)
+  : opts_{opts}
+{
+    at::ScalarType dtype = opts_.maybe_dtype().value_or(at::kFloat);
+    if (dtype != at::kFloat && dtype != at::kByte)
+        throw_<not_supported_error>(
+            "`image_decoder` supports only `torch.float32` and `torch.uint8` data types.");
+}
+
+
+};
