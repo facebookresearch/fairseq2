87f7ca67ae2751ce7001a7910f2fc17f4cee5f82
Can Balioglu <cbalioglu@users.noreply.github.com>
2023-11-28 14:03:11 -0800

    Introduce LLaMA and Mistral chatbots

--- src/fairseq2/models/llama/tokenizer.py
+++ src/fairseq2/models/llama/tokenizer.py
@@ -7,8 +7,16 @@
 from typing import final
 
 from fairseq2.data.text import BasicSentencePieceTokenizer
+from fairseq2.data.typing import PathLike
 
 
 @final
 class LLaMATokenizer(BasicSentencePieceTokenizer):
     """Represents the tokenizer used by LLaMA models."""
+
+    def __init__(self, pathname: PathLike) -> None:
+        """
+        :param pathname:
+            The pathname of the SentencePiece model file.
+        """
+        super().__init__(pathname)
