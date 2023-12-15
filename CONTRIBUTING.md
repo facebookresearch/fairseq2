510f85fd1580cb1426ac4e5f996ac2378dafeb9f
Can Balioglu <cbalioglu@users.noreply.github.com>
2023-12-14 16:58:12 -0800

    Update

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -55,7 +55,7 @@ pip install -r requirements-devel.txt
 > the fairseq2n installation command above to get the most up-to-date binary. If
 > you observe runtime or test failures after the installation, it might be
 > because the latest nightlies are not published yet. If the problem persists
-> after about 12 hours, please create a
+> for more than 12 hours, please create a
 > [GitHub issue](https://github.com/facebookresearch/fairseq2/issues/new/choose).
 
 ## Testing Your Work
--- native/python/setup.py
+++ native/python/setup.py
@@ -8,7 +8,6 @@ from os import path
 from typing import Final, List, Optional
 
 import torch
-from packaging import version
 from setuptools import Command, find_packages, setup
 from setuptools.command.install import install as install_base
 from setuptools.dist import Distribution as DistributionBase
@@ -168,6 +167,6 @@ setup(
         # PyTorch has no ABI compatibility between releases; this means we have
         # to ensure that we depend on the exact same version that we used to
         # build fairseq2n.
-        "torch==" + version.parse(torch.__version__).public,
+        "torch==" + torch.__version__.split("+", 1)[0],  # Trim the label.
     ],
 )
