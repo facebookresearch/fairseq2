6017739ff97ebc0d3dbfec7f7e2ed1d33e5a5274
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-04-16 13:27:12 -0400

    Introduce sharded projection and embedding modules (#458)

--- src/fairseq2/nn/projection.py
+++ src/fairseq2/nn/projection.py
@@ -17,7 +17,10 @@ from torch.nn import Module
 from torch.nn.functional import linear
 from torch.nn.parameter import Parameter
 
-from fairseq2.typing import DataType, Device, override
+from fairseq2.collective import gather, reduce, reduce_on_backward, scatter
+from fairseq2.gang import Gang
+from fairseq2.nn.utils.module import to_empty
+from fairseq2.typing import META, DataType, Device, override
 
 
 class Projection(Module, ABC):
@@ -113,7 +116,7 @@ class Linear(Projection):
         if self.init_fn is not None:
             self.init_fn(self)
         else:
-            _init_uniform_weight_bias(self.weight, self.bias)
+            _init_uniform_weight_and_bias(self.weight, self.bias)
 
     @override
     def forward(self, x: Tensor) -> Tensor:
@@ -124,6 +127,353 @@ class Linear(Projection):
         s = super().extra_repr()
 
         s = f"{s}, bias={self.bias is not None}"
+
+        if self.init_fn is not None:
+            init_fn = getattr(self.init_fn, "__name__", self.init_fn)
+
+            s = f"{s}, init_fn={init_fn}"
+
+        return s
+
+
+@final
+class ColumnShardedLinear(Projection):
+    """Represents a :class:`Linear` that is sharded across its output dimension."""
+
+    gang: Gang
+    sharded_output_dim: int
+    weight: Parameter
+    bias: Optional[Parameter]
+    init_fn: Optional[Callable[[Linear], None]]
+
+    @staticmethod
+    def from_linear(linear: Linear, gang: Gang) -> ColumnShardedLinear:
+        """Construct a :class:`ColumnShardedLinear` by sharding ``linear``.
+
+        :param linear:
+            The projection to shard.
+        :param gang:
+            The gang over which to shard ``linear``.
+        """
+        sharded = ColumnShardedLinear(
+            gang,
+            linear.input_dim,
+            linear.output_dim,
+            bias=linear.bias is not None,
+            init_fn=linear.init_fn,
+            device=META,
+            dtype=linear.weight.dtype,
+        )
+
+        to_empty(sharded, gang.device)
+
+        sharded._copy_weight_and_bias(linear)
+
+        return sharded
+
+    def __init__(
+        self,
+        gang: Gang,
+        input_dim: int,
+        output_dim: int,
+        bias: bool,
+        *,
+        init_fn: Optional[Callable[[Linear], None]] = None,
+        device: Optional[Device] = None,
+        dtype: Optional[DataType] = None,
+    ) -> None:
+        """
+        :param gang:
+            The gang over which to shard the weight tensor.
+        :param input_dim:
+            The dimensionality of inputs.
+        :param output_dim:
+            The dimensionality of projected outputs.
+        :param bias:
+            If ``True``, learns an additive bias.
+        :param init_fn:
+            The callable to use for parameter initialization.
+        """
+        super().__init__(input_dim, output_dim)
+
+        if output_dim % gang.size != 0:
+            raise ValueError(
+                f"`output_dim` must be divisible by `gang.size` ({gang.size}), but is {output_dim} instead."
+            )
+
+        self.gang = gang
+
+        self.sharded_output_dim = output_dim // gang.size
+
+        if device is None:
+            device = gang.device
+        elif device != gang.device and device != META:
+            raise ValueError(
+                "`device` must either match `gang.device` or must be of type `meta`."
+            )
+
+        weight = torch.empty(
+            (self.sharded_output_dim, input_dim), device=device, dtype=dtype
+        )
+
+        self.weight = Parameter(weight)
+
+        if bias:
+            self.bias = Parameter(
+                torch.empty((self.sharded_output_dim,), device=device, dtype=dtype)
+            )
+        else:
+            self.register_parameter("bias", None)
+
+        self.init_fn = init_fn
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        """Reset the parameters and buffers of the module."""
+        linear = self._linear_like(self.gang.device)
+
+        self._copy_weight_and_bias(linear)
+
+    def _copy_weight_and_bias(self, linear: Linear) -> None:
+        with torch.no_grad():
+            weight_shards = linear.weight.split(self.sharded_output_dim)
+
+            self.weight.copy_(weight_shards[self.gang.rank])
+
+        if self.bias is not None:
+            assert linear.bias is not None
+
+            with torch.no_grad():
+                bias_shards = linear.bias.split(self.sharded_output_dim)
+
+                self.bias.copy_(bias_shards[self.gang.rank])
+
+    @override
+    def forward(self, x: Tensor) -> Tensor:
+        x = reduce_on_backward(x, self.gang)
+
+        x = linear(x, self.weight, self.bias)
+
+        x = gather(x, self.gang, dim=-1)
+
+        return x
+
+    def to_linear(self, device: Optional[Device] = None) -> Linear:
+        """Convert this instance to a :class:`Linear`."""
+        linear = self._linear_like(META)
+
+        to_empty(linear, device or self.gang.device)
+
+        with torch.no_grad():
+            weight = gather(self.weight, self.gang, dim=0)
+
+            linear.weight.copy_(weight)
+
+        if self.bias is not None:
+            assert linear.bias is not None
+
+            with torch.no_grad():
+                bias = gather(self.bias, self.gang, dim=0)
+
+                linear.bias.copy_(bias)
+
+        return linear
+
+    def _linear_like(self, device: Device) -> Linear:
+        return Linear(
+            self.input_dim,
+            self.output_dim,
+            bias=self.bias is not None,
+            init_fn=self.init_fn,
+            device=device,
+            dtype=self.weight.dtype,
+        )
+
+    def extra_repr(self) -> str:
+        """:meta private:"""
+        s = super().extra_repr()
+
+        s = (
+            f"{s}, "
+            f"bias={self.bias is not None}, "
+            f"sharded_output_dim={self.sharded_output_dim}, "
+            f"rank={self.gang.rank}, "
+            f"world_size={self.gang.size}"
+        )
+
+        if self.init_fn is not None:
+            init_fn = getattr(self.init_fn, "__name__", self.init_fn)
+
+            s = f"{s}, init_fn={init_fn}"
+
+        return s
+
+
+@final
+class RowShardedLinear(Projection):
+    """Represents a :class:`Linear` that is sharded across its input dimension."""
+
+    gang: Gang
+    sharded_input_dim: int
+    weight: Parameter
+    bias: Optional[Parameter]
+    init_fn: Optional[Callable[[Linear], None]]
+
+    @staticmethod
+    def from_linear(linear: Linear, gang: Gang) -> RowShardedLinear:
+        """Construct a :class:`RowShardedLinear` by sharding ``linear``.
+
+        :param linear:
+            The projection to shard.
+        :param gang:
+            The gang over which to shard ``linear``.
+        """
+        sharded = RowShardedLinear(
+            gang,
+            linear.input_dim,
+            linear.output_dim,
+            bias=linear.bias is not None,
+            init_fn=linear.init_fn,
+            device=META,
+            dtype=linear.weight.dtype,
+        )
+
+        to_empty(sharded, gang.device)
+
+        sharded._copy_weight_and_bias(linear)
+
+        return sharded
+
+    def __init__(
+        self,
+        gang: Gang,
+        input_dim: int,
+        output_dim: int,
+        bias: bool,
+        *,
+        init_fn: Optional[Callable[[Linear], None]] = None,
+        device: Optional[Device] = None,
+        dtype: Optional[DataType] = None,
+    ) -> None:
+        """
+        :param gang:
+            The gang over which to shard the weight tensor.
+        :param input_dim:
+            The dimensionality of inputs.
+        :param output_dim:
+            The dimensionality of projected outputs.
+        :param bias:
+            If ``True``, learns an additive bias.
+        :param init_fn:
+            The callable to use for parameter initialization.
+        """
+        super().__init__(input_dim, output_dim)
+
+        if input_dim % gang.size != 0:
+            raise ValueError(
+                f"`input_dim` must be divisible by `gang.size` ({gang.size}), but is {input_dim} instead."
+            )
+
+        self.gang = gang
+
+        self.sharded_input_dim = input_dim // gang.size
+
+        if device is None:
+            device = gang.device
+        elif device != gang.device and device != META:
+            raise ValueError(
+                "`device` must either match `gang.device` or must be of type `meta`."
+            )
+
+        weight = torch.empty(
+            (output_dim, self.sharded_input_dim), device=device, dtype=dtype
+        )
+
+        self.weight = Parameter(weight)
+
+        if bias:
+            self.bias = Parameter(
+                torch.empty((output_dim,), device=device, dtype=dtype)
+            )
+        else:
+            self.register_parameter("bias", None)
+
+        self.init_fn = init_fn
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        """Reset the parameters and buffers of the module."""
+        linear = self._linear_like(self.gang.device)
+
+        self._copy_weight_and_bias(linear)
+
+    def _copy_weight_and_bias(self, linear: Linear) -> None:
+        with torch.no_grad():
+            weight_shards = linear.weight.split(self.sharded_input_dim, dim=1)
+
+            self.weight.copy_(weight_shards[self.gang.rank])
+
+        if self.bias is not None:
+            assert linear.bias is not None
+
+            with torch.no_grad():
+                self.bias.copy_(linear.bias)
+
+    @override
+    def forward(self, x: Tensor) -> Tensor:
+        x = scatter(x, self.gang, dim=-1)
+
+        x = linear(x, self.weight)
+
+        x = reduce(x, self.gang)
+
+        if self.bias is not None:
+            x = x + self.bias
+
+        return x
+
+    def to_linear(self, device: Optional[Device] = None) -> Linear:
+        """Convert this instance to a :class:`Linear`."""
+        linear = self._linear_like(META)
+
+        to_empty(linear, device or self.gang.device)
+
+        with torch.no_grad():
+            weight = gather(self.weight, self.gang, dim=1)
+
+            linear.weight.copy_(weight)
+
+        if self.bias is not None:
+            assert linear.bias is not None
+
+            with torch.no_grad():
+                linear.bias.copy_(self.bias)
+
+        return linear
+
+    def _linear_like(self, device: Device) -> Linear:
+        return Linear(
+            self.input_dim,
+            self.output_dim,
+            bias=self.bias is not None,
+            init_fn=self.init_fn,
+            device=device,
+            dtype=self.weight.dtype,
+        )
+
+    def extra_repr(self) -> str:
+        """:meta private:"""
+        s = super().extra_repr()
+
+        s = (
+            f"{s}, "
+            f"bias={self.bias is not None}, "
+            f"sharded_input_dim={self.sharded_input_dim}, "
+            f"rank={self.gang.rank}, "
+            f"world_size={self.gang.size}"
+        )
 
         if self.init_fn is not None:
             init_fn = getattr(self.init_fn, "__name__", self.init_fn)
@@ -158,7 +508,7 @@ class TiedProjection(Projection):
         return linear(x, self.weight, self.bias)
 
 
-def _init_uniform_weight_bias(weight: Tensor, bias: Optional[Tensor]) -> None:
+def _init_uniform_weight_and_bias(weight: Tensor, bias: Optional[Tensor]) -> None:
     nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
 
     if bias is not None:
