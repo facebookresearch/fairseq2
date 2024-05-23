0e3a5403b7ac8ec1a059a21184e86917df162fd9
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-05-23 13:51:03 -0400

    Revise CLI API (#538)

--- src/fairseq2/recipes/lm/__init__.py
+++ src/fairseq2/recipes/lm/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from fairseq2.recipes.cli import Cli, CliGroup, RecipeCommand
+from fairseq2.recipes.cli import Cli, RecipeCommandHandler
 from fairseq2.recipes.lm.instruction_finetune import (
     InstructionFinetuneConfig as InstructionFinetuneConfig,
 )
@@ -17,16 +17,14 @@ from fairseq2.recipes.lm.instruction_finetune import (
 
 
 def _setup_lm_cli(cli: Cli) -> None:
-    cli_group = CliGroup(name="lm", help="Language Model recipes")
+    group = cli.add_group("lm", help="Language Model recipes")
 
-    cli.register_group(cli_group)
-
-    instruction_finetune_cmd = RecipeCommand(
-        name="instruction-finetune",
-        help="instruction-finetune a Language Model",
+    handler = RecipeCommandHandler(
         loader=load_instruction_finetuner,
         preset_configs=instruction_finetune_presets,
         default_preset="llama2_7b_chat",
     )
 
-    cli_group.register_command(instruction_finetune_cmd)
+    group.add_command(
+        "instruction_finetune", handler, help="instruction-finetune a Language Model"
+    )
--- src/fairseq2/recipes/wav2vec2/asr/__init__.py
+++ src/fairseq2/recipes/wav2vec2/asr/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from fairseq2.recipes.cli import Cli, CliGroup, RecipeCommand
+from fairseq2.recipes.cli import Cli, CliCommandHandler, RecipeCommandHandler
 from fairseq2.recipes.wav2vec2.asr.eval import (
     load_wav2vec2_asr_evaluator,
     wav2vec2_asr_eval_presets,
@@ -16,26 +16,22 @@ from fairseq2.recipes.wav2vec2.asr.train import (
 
 
 def _setup_wav2vec2_asr_cli(cli: Cli) -> None:
-    cli_group = CliGroup(name="wav2vec2_asr", help="wav2vec 2.0 ASR recipes")
+    group = cli.add_group("wav2vec2_asr", help="wav2vec 2.0 ASR recipes")
 
-    cli.register_group(cli_group)
+    handler: CliCommandHandler
 
-    train_cmd = RecipeCommand(
-        name="train",
-        help="train a wav2vec 2.0 ASR model",
-        loader=load_wav2vec2_asr_trainer,
+    handler = RecipeCommandHandler(
+        load_wav2vec2_asr_trainer,
         preset_configs=wav2vec2_asr_train_presets,
         default_preset="base_10h",
     )
 
-    cli_group.register_command(train_cmd)
+    group.add_command("train", handler, help="train a wav2vec 2.0 ASR model")
 
-    eval_cmd = RecipeCommand(
-        name="eval",
-        help="evaluate a wav2vec 2.0 ASR model",
-        loader=load_wav2vec2_asr_evaluator,
+    handler = RecipeCommandHandler(
+        load_wav2vec2_asr_evaluator,
         preset_configs=wav2vec2_asr_eval_presets,
         default_preset="base_10h",
     )
 
-    cli_group.register_command(eval_cmd)
+    group.add_command("eval", handler, help="evaluate a wav2vec 2.0 ASR model")
