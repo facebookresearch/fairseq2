1e36968e79496af62288f3bf6725293ca0b38783
Tuan Tran <{ID}+{username}@users.noreply.github.com>
2024-06-18 15:24:01 +0200

    restructure

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -10,7 +10,7 @@ from torcheval.metrics import Mean, Sum
 
 from fairseq2.gang import FakeGang
 from fairseq2.metrics import MetricBag
-from fairseq2.metrics.hf import HFMetric
+from fairseq2.recipes.hf.metrics import HFMetric
 from tests.common import device
 
 
@@ -74,28 +74,3 @@ class TestMetricBag:
             match=r"^`state_dict` must contain metrics \['test1', 'test2'\], but contains \['foo'\] instead\.$",
         ):
             bag.load_state_dict(state_dict)
-
-    def test_hf_metric(self) -> None:
-        bag = MetricBag(gang=FakeGang(device=device))
-        bag.hf_accuracy = HFMetric("accuracy")
-
-        # All compute arguments are registered in the beginning
-        bag.f1 = HFMetric("f1", average="macro")
-
-        references = [[0, 1, 2], [0, 1], [2], [3]]
-        predictions = [[0, 1, 1], [2, 1], [0], [3]]
-
-        bag.begin_updates()
-        for p, r in zip(predictions, references):
-            bag.hf_accuracy.update(predictions=p, references=r)
-            bag.f1.update(predictions=p, references=r)
-        bag.commit_updates()
-
-        bag.auto_sync = True
-        result = bag.sync_and_compute_metrics()
-        assert result
-        assert (
-            "hf_accuracy" in result
-            and pytest.approx(result["hf_accuracy"].item(), 0.0001) == 0.5714
-        )
-        assert "f1" in result and pytest.approx(result["f1"].item(), 0.001) == 0.575
