6f67ab3f257f7dadb27d72d1945709bfc04208ab
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-07-03 23:21:58 +0000

    Introduce language model generate command

--- src/fairseq2/recipes/lm/__init__.py
+++ src/fairseq2/recipes/lm/__init__.py
@@ -10,17 +10,20 @@ from fairseq2.recipes.lm.instruction_finetune import (
     instruction_finetune_presets,
     load_instruction_finetuner,
 )
+from fairseq2.recipes.lm.text_generate import load_text_generator, text_generate_presets
 
 
 def _setup_lm_cli(cli: Cli) -> None:
     group = cli.add_group("lm", help="language model recipes")
 
+    # Chatbot
     group.add_command(
         name="chatbot",
         handler=ChatbotCommandHandler(),
         help="run a terminal-based chatbot demo",
     )
 
+    # Instruction Finetune
     instruction_finetune_handler = RecipeCommandHandler(
         loader=load_instruction_finetuner,
         preset_configs=instruction_finetune_presets,
@@ -31,4 +34,17 @@ def _setup_lm_cli(cli: Cli) -> None:
         name="instruction_finetune",
         handler=instruction_finetune_handler,
         help="instruction-finetune a language model",
+    )
+
+    # Text Generate
+    text_generate_handler = RecipeCommandHandler(
+        loader=load_text_generator,
+        preset_configs=text_generate_presets,
+        default_preset="llama3_8b_instruct",
+    )
+
+    group.add_command(
+        name="generate",
+        handler=text_generate_handler,
+        help="generate text",
     )
