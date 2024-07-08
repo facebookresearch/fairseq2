253c769625107c8eca81a3da1a65879c84d6b585
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-07-08 14:30:30 +0000

    Introduce GenericParallelTextDataset

--- src/fairseq2/datasets/__init__.py
+++ src/fairseq2/datasets/__init__.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from fairseq2.datasets.batching import LengthBatching as LengthBatching
+from fairseq2.datasets.batching import StaticBatching as StaticBatching
 from fairseq2.datasets.data_reader import DataPipelineReader as DataPipelineReader
 from fairseq2.datasets.data_reader import DataReader as DataReader
 from fairseq2.datasets.error import DatasetError as DatasetError
