7e7a3eea6d57c3fb39a2f5aed3756bfd624cdd38
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-07-09 15:45:55 -0400

    Allow passing dataset path to first-party recipes (#652)

--- src/fairseq2/recipes/wav2vec2/asr/train.py
+++ src/fairseq2/recipes/wav2vec2/asr/train.py
@@ -14,13 +14,13 @@ import torch
 from torch import Tensor
 from torch.nn import Module
 
-from fairseq2.assets import default_asset_store
+from fairseq2.assets import AssetNotFoundError, default_asset_store
 from fairseq2.assets.utils import retrieve_asset_card
 from fairseq2.checkpoint import CheckpointModelMetadataProvider, FileCheckpointManager
 from fairseq2.config_registry import ConfigRegistry
 from fairseq2.data.text import load_text_tokenizer
 from fairseq2.datasets import LengthBatching
-from fairseq2.datasets.asr import load_asr_dataset
+from fairseq2.datasets.asr import GenericAsrDataset, load_asr_dataset
 from fairseq2.gang import Gang
 from fairseq2.logging import get_log_writer
 from fairseq2.models.seq2seq import Seq2SeqBatch
@@ -63,7 +63,7 @@ class Wav2Vec2AsrTrainConfig:
 
     # Data
     dataset: Union[str, Path] = "librilight_asr_10h"
-    """The name or path to the asset card of the ASR dataset."""
+    """The name, path, or path to the asset card of the ASR dataset."""
 
     train_split: str = "train"
     """The name of the train data split."""
@@ -235,13 +235,26 @@ def load_wav2vec2_asr_trainer(
     log.info("Tokenizer loaded.")
 
     # Load the dataset.
-    dataset_card = retrieve_asset_card(config.dataset)
+    try:
+        dataset_card = retrieve_asset_card(config.dataset)
+    except AssetNotFoundError:
+        dataset_card = None
 
-    log.info("Loading {} ASR dataset.", dataset_card.name)
+    if dataset_card is not None:
+        log.info("Loading {} ASR dataset.", dataset_card.name)
 
-    dataset = load_asr_dataset(config.dataset)
+        dataset = load_asr_dataset(dataset_card)
 
-    log.info("Dataset loaded.")
+        log.info("Dataset loaded.")
+    else:
+        try:
+            path = Path(config.dataset)
+        except ValueError:
+            raise AssetNotFoundError(
+                config.dataset, f"An asset with the name '{config.dataset}' cannot be found."  # type: ignore[arg-type]
+            )
+
+        dataset = GenericAsrDataset.from_path(path)
 
     # Initialize the model configuration.
     if config.model_arch is None:
