e2649474d142a78a14e576f587f6a9172c5ab896
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-07-18 11:17:20 -0400

    Introduce machine translation train recipe (#687)

--- src/fairseq2/recipes/mt/__init__.py
+++ src/fairseq2/recipes/mt/__init__.py
@@ -6,6 +6,7 @@
 
 from fairseq2.recipes.cli import Cli, RecipeCommandHandler
 from fairseq2.recipes.mt.eval import load_mt_evaluator, mt_eval_presets
+from fairseq2.recipes.mt.train import load_mt_trainer, mt_train_presets
 from fairseq2.recipes.mt.translate import load_text_translator, text_translate_presets
 from fairseq2.recipes.utils.sweep import default_sweep_tagger
 
@@ -14,6 +15,19 @@ def _setup_mt_cli(cli: Cli) -> None:
     default_sweep_tagger.extend_allow_set("source_lang", "target_lang")
 
     group = cli.add_group("mt", help="machine translation recipes")
+
+    # Train
+    train_handler = RecipeCommandHandler(
+        loader=load_mt_trainer,
+        preset_configs=mt_train_presets,
+        default_preset="nllb_dense_600m",
+    )
+
+    group.add_command(
+        name="train",
+        handler=train_handler,
+        help="train a machine translation model",
+    )
 
     # Eval
     eval_handler = RecipeCommandHandler(
