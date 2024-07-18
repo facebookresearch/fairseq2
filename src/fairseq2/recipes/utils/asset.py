0c386733fd6f4eceaf37fede78d50ec372c02d8e
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-07-18 14:55:05 +0000

    Nit update to Trainer

--- src/fairseq2/recipes/utils/asset.py
+++ src/fairseq2/recipes/utils/asset.py
@@ -27,6 +27,9 @@ def retrieve_asset_card(name_or_card: Union[str, AssetCard, Path]) -> AssetCard:
         return name_or_card
 
     if isinstance(name_or_card, Path):
+        if name_or_card.is_dir():
+            raise AssetNotFoundError(f"{name_or_card}", "The asset cannot be found.")
+
         return _card_from_file(name_or_card)
 
     name = name_or_card
