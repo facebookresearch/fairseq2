5d2b558c8b972e3d1965eb221fbc0e8f4c899214
Ahmed Saed <37080003+Ahmedsaed@users.noreply.github.com>
2024-07-26 23:34:22 +0000

    Correctly sort imports

--- src/fairseq2/recipes/eval/asr.py
+++ src/fairseq2/recipes/eval/asr.py
@@ -11,7 +11,10 @@ from pathlib import Path
 from typing import Optional, cast
 
 import torch
-from datasets import Dataset, load_dataset  # type: ignore[attr-defined,import-untyped,import-not-found]
+from datasets import (  # type: ignore[attr-defined,import-untyped,import-not-found]
+    Dataset,
+    load_dataset,
+)
 
 from fairseq2.data.data_pipeline import SequenceData
 from fairseq2.data.text import load_text_tokenizer
