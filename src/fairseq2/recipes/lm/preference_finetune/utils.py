cf9a46683e3977e8e4ab432d580dd59fcc6fe973
Andrew Nakamoto <nakamoto@meta.com>
2024-08-26 17:02:20 +0000

    utils.py update_sequence_lengths bugfix

--- src/fairseq2/recipes/lm/preference_finetune/utils.py
+++ src/fairseq2/recipes/lm/preference_finetune/utils.py
@@ -135,10 +135,10 @@ class PreferenceFinetuneMetricBag(SequenceMetricBag):
             The batch processed by the model.
         """
         self._chosen_lengths.update(
-            batch.chosen.num_target_elements() / batch.chosen.batch_size,
+            Tensor(batch.chosen.num_target_elements()) / batch.chosen.batch_size,
             weight=batch.chosen.batch_size,
         )
         self._rejected_lengths.update(
-            batch.rejected.num_target_elements() / batch.rejected.batch_size,
+            Tensor(batch.rejected.num_target_elements()) / batch.rejected.batch_size,
             weight=batch.rejected.batch_size,
         )
