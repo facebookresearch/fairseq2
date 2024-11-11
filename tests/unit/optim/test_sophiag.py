5f30548dbbfa0b16a3366d87cd10f9f54afc3777
Eric Brown <ebrown5676@gmail.com>
2024-11-11 08:45:04 -0800

    Create test_sophiag.py

---
+++ tests/unit/optim/test_sophiag.py
@@ -0,0 +1,19 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from torch.nn import Module
+
+
+class SophiaGTestNet(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class TestSophiaG:
+    def test_step_updates_params_correctly(self) -> None:
+        pass
