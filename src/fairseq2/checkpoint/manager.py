b317434c614600ec15b6c4c8c3eab8c6dd7fde72
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-12-10 15:19:37 +0000

    Add additional FSDP suppressions

--- src/fairseq2/checkpoint/manager.py
+++ src/fairseq2/checkpoint/manager.py
@@ -6,6 +6,7 @@
 
 from __future__ import annotations
 
+import warnings
 from abc import ABC, abstractmethod
 from collections.abc import Iterator, Mapping, Set
 from contextlib import AbstractContextManager, nullcontext
@@ -13,6 +14,7 @@ from pathlib import Path
 from pickle import PickleError
 from shutil import rmtree
 from typing import Any, NoReturn, final
+from warnings import catch_warnings
 
 import yaml
 from torch.distributed._shard import load_with_process_group
@@ -443,12 +445,17 @@ class FileCheckpointManager(CheckpointManager):
 
         log.info("Extracting consolidated model state.")
 
-        with FSDP.state_dict_type(
-            model,
-            StateDictType.FULL_STATE_DICT,
-            state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=True),
-        ):
-            state_dict = model.state_dict()
+        with catch_warnings():
+            warnings.simplefilter("ignore")  # Suppress noisy FSDP warnings.
+
+            with FSDP.state_dict_type(
+                model,
+                StateDictType.FULL_STATE_DICT,
+                state_dict_config=FullStateDictConfig(
+                    offload_to_cpu=True, rank0_only=True
+                ),
+            ):
+                state_dict = model.state_dict()
 
         self._root_gang.barrier()
 
