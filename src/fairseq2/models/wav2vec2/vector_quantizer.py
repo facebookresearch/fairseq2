688bc227041634f9fe8fea62620233ce9f8a587e
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-12-12 15:43:44 -0500

    Refactor wav2vec2 masker (#830)

--- src/fairseq2/models/wav2vec2/vector_quantizer.py
+++ src/fairseq2/models/wav2vec2/vector_quantizer.py
@@ -8,7 +8,6 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import final
 
 import torch
 import torch.nn as nn
@@ -62,7 +61,6 @@ class VectorQuantizerOutput(ABC):
         pass
 
 
-@final
 class GumbelVectorQuantizer(VectorQuantizer):
     """Quantizes incoming data using Gumbel-Softmax."""
 
@@ -144,7 +142,7 @@ class GumbelVectorQuantizer(VectorQuantizer):
         self.num_updates.zero_()
 
     @override
-    def forward(self, x: Tensor) -> "GumbelVectorQuantizerOutput":
+    def forward(self, x: Tensor) -> GumbelVectorQuantizerOutput:
         current_temp = self._compute_current_temp()
 
         bsz, tsz, fsz = x.shape
@@ -228,7 +226,6 @@ def init_entry_projection(proj: Linear) -> None:
     nn.init.zeros_(proj.bias)
 
 
-@final
 @dataclass
 class GumbelVectorQuantizerOutput(VectorQuantizerOutput):
     cb: Tensor
