a63cc23779d24a239deda46d8528d2f33d0767b2
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-12-13 15:19:12 -0500

    Revise determine_default_device (#904)

--- src/fairseq2/device.py
+++ src/fairseq2/device.py
@@ -10,12 +10,14 @@ import os
 
 import torch
 
-from fairseq2.logging import get_log_writer
+from fairseq2.error import InternalError
+from fairseq2.logging import log
 from fairseq2.typing import CPU, Device
-from fairseq2.utils.env import get_int_from_env
-
-log = get_log_writer(__name__)
-
+from fairseq2.utils.env import (
+    InvalidEnvironmentVariableError,
+    get_device_from_env,
+    get_int_from_env,
+)
 
 _default_device: Device | None = None
 
@@ -27,14 +29,12 @@ def determine_default_device() -> Device:
     if _default_device is not None:
         return _default_device
 
-    device_str = os.environ.get("FAIRSEQ2_DEVICE")
-    if device_str is not None:
-        try:
-            _default_device = Device(device_str)
-        except RuntimeError as ex:
-            raise RuntimeError(
-                f"The value of the `FAIRSEQ2_DEVICE` environment variable must specify a valid PyTorch device, but is '{device_str}' instead."
-            ) from ex
+    try:
+        _default_device = get_device_from_env("FAIRSEQ2_DEVICE")
+    except InvalidEnvironmentVariableError as ex:
+        raise DeviceDetectionError(
+            "The default device cannot be set using the `FAIRSEQ2_DEVICE` environment variable. See the nested exception for details."
+        ) from ex
 
     if _default_device is None:
         _default_device = determine_default_cuda_device()
@@ -73,7 +73,12 @@ def determine_default_cuda_device() -> Device | None:
         device = None
 
     if device is None:
-        idx = _get_device_index(num_devices, device_type="cuda")
+        try:
+            idx = _get_device_index(num_devices, device_type="cuda")
+        except InvalidEnvironmentVariableError as ex:
+            raise DeviceDetectionError(
+                "The default `cuda` device index cannot be inferred from the environment. See the nested exception for details."
+            ) from ex
 
         device = Device("cuda", index=idx)
 
@@ -81,7 +86,8 @@ def determine_default_cuda_device() -> Device | None:
 
 
 def _get_device_index(num_devices: int, device_type: str) -> int:
-    assert num_devices > 0
+    if num_devices <= 0:
+        raise InternalError(f"`num_devices` is {num_devices}.")
 
     # We use the `LOCAL_RANK` environment variable to determine which device to
     # pick in case the process has more than one available.
@@ -89,20 +95,24 @@ def _get_device_index(num_devices: int, device_type: str) -> int:
     if device_idx is None:
         num_procs = get_int_from_env("LOCAL_WORLD_SIZE")
         if num_procs is not None and num_procs > 1 and num_devices > 1:
-            raise RuntimeError(
+            raise InvalidEnvironmentVariableError(
                 f"The default `{device_type}` device cannot be determined. There are {num_devices} devices available, but the `LOCAL_RANK` environment variable is not set."
             )
 
         return 0
 
     if device_idx < 0:
-        raise RuntimeError(
-            f"The value of the `LOCAL_RANK` environment variable must be greater than or equal to 0, but is {device_idx} instead."
+        raise InvalidEnvironmentVariableError(
+            f"The value of the `LOCAL_RANK` environment variable is expected to be greater than or equal to 0, but is {device_idx} instead."
         )
 
     if device_idx >= num_devices:
-        raise RuntimeError(
-            f"The value of the `LOCAL_RANK` environment variable must be less than the number of available `{device_type}` devices ({num_devices}), but is {device_idx} instead."
+        raise InvalidEnvironmentVariableError(
+            f"The value of the `LOCAL_RANK` environment variable is expected to be less than the number of available `{device_type}` devices ({num_devices}), but is {device_idx} instead."
         )
 
     return device_idx
+
+
+class DeviceDetectionError(Exception):
+    pass
