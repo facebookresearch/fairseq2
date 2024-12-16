0bbb7ceceded7a4d3f47ac56e6d631010058ee8c
Can Balioglu <cbalioglu@users.noreply.github.com>
2024-12-16 14:25:37 -0500

    Nit updates to recipe logging (#917)

--- src/fairseq2/recipes/__init__.py
+++ src/fairseq2/recipes/__init__.py
@@ -6,47 +6,76 @@
 
 from __future__ import annotations
 
-from importlib_metadata import entry_points
+import os
+import sys
+from signal import SIG_DFL, SIGINT, raise_signal, signal
+from typing import Iterator
 
+import torch
+from importlib_metadata import entry_points
+from torch.cuda import OutOfMemoryError
+
+from fairseq2.error import ContractError, InternalError
+from fairseq2.logging import log
 from fairseq2.recipes.cli import Cli
 
 # isort: split
 
-import os
-
-from fairseq2.logging import get_log_writer
 from fairseq2.recipes.assets import _setup_asset_cli
 from fairseq2.recipes.hg import _setup_hg_cli
 from fairseq2.recipes.llama import _setup_llama_cli
 from fairseq2.recipes.lm import _setup_lm_cli
 from fairseq2.recipes.logging import setup_basic_logging
 from fairseq2.recipes.mt import _setup_mt_cli
-from fairseq2.recipes.utils.log import exception_logger
 from fairseq2.recipes.wav2vec2 import _setup_wav2vec2_cli
 from fairseq2.recipes.wav2vec2.asr import _setup_wav2vec2_asr_cli
-
-log = get_log_writer(__name__)
 
 
 def main() -> None:
     """Run the command line fairseq2 program."""
+    exit_code = 1
+
+    try:
+        _run()
+
+        exit_code = 0
+    except KeyboardInterrupt:
+        log.info("The command has been canceled!")
+
+        signal(SIGINT, SIG_DFL)
+
+        raise_signal(SIGINT)
+    except OutOfMemoryError:
+        s = torch.cuda.memory_summary()
+
+        log.exception("CUDA run out of memory. See the logged memory stats.\n{}", s)
+    except InternalError:
+        log.exception("The command has failed with an unexpected internal error. Please file a bug report.")  # fmt: skip
+    except ContractError:
+        log.exception("The command has failed with an unexpected internal error caused by an extension. See the logged stack trace for details and file a bug report to the corresponding extension author.")  # fmt: skip
+    except Exception:
+        log.exception("The command has failed with an unexpected error. See the logged stack trace for details.")  # fmt: skip
+
+    sys.exit(exit_code)
+
+
+def _run() -> None:
     from fairseq2 import __version__, setup_extensions
 
-    with exception_logger(log):
-        setup_basic_logging()
+    setup_basic_logging()
 
-        setup_extensions()
+    setup_extensions()
 
-        cli = Cli(
-            name="fairseq2",
-            origin_module="fairseq2",
-            version=__version__,
-            description="command line interface of fairseq2",
-        )
+    cli = Cli(
+        name="fairseq2",
+        origin_module="fairseq2",
+        version=__version__,
+        description="command line interface of fairseq2",
+    )
 
-        _setup_cli(cli)
+    _setup_cli(cli)
 
-        cli.run()
+    cli.run()
 
 
 def _setup_cli(cli: Cli) -> None:
