8c1801bd8e517c65bb3cb217e572ebc9926fb71a
Kaushik Ram Sadagopan <kaushikram2811@gmail.com>
2024-12-26 22:51:24 +0000

    Add wav2vec2 archs, encoder archs, asr archs for 3b, 3.25b.

--- src/fairseq2/models/wav2vec2/asr/archs.py
+++ src/fairseq2/models/wav2vec2/asr/archs.py
@@ -76,3 +76,98 @@ def _large_lv60k_100h() -> Wav2Vec2AsrConfig:
     config.max_spatial_mask_prob = 0.55
 
     return config
+
+
+@wav2vec2_asr_arch("300m_bib61")
+def _300m_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("large_lv60k")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.0
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
+
+
+@wav2vec2_asr_arch("1b_bib61")
+def _1b_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("1b")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.0
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
+
+
+@wav2vec2_asr_arch("2b_bib61")
+def _2b_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("2b")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.0
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
+
+
+@wav2vec2_asr_arch("3b_bib61")
+def _3b_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("3b")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.0
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
+
+
+@wav2vec2_asr_arch("3.25b_bib61")
+def _3b_higher_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("3.25b")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.0
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
