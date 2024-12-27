b967ead405dc984c44c4a12f419a52f827e9ffa9
Kaushik Ram Sadagopan <kaushikram2811@gmail.com>
2024-12-27 23:23:44 +0000

    Add back layer_drop to asr archs.

--- src/fairseq2/models/wav2vec2/asr/archs.py
+++ src/fairseq2/models/wav2vec2/asr/archs.py
@@ -87,7 +87,7 @@ def _300m_bib61() -> Wav2Vec2AsrConfig:
     config.encoder_config.dropout_p = 0.0
     config.encoder_config.attn_dropout_p = 0.0
     config.encoder_config.ffn_inner_dropout_p = 0.1
-    config.encoder_config.layer_drop_p = 0.0
+    config.encoder_config.layer_drop_p = 0.1
 
     config.use_masking = False
     config.max_temporal_mask_prob = 0.0
@@ -106,7 +106,26 @@ def _1b_bib61() -> Wav2Vec2AsrConfig:
     config.encoder_config.dropout_p = 0.0
     config.encoder_config.attn_dropout_p = 0.0
     config.encoder_config.ffn_inner_dropout_p = 0.1
-    config.encoder_config.layer_drop_p = 0.0
+    config.encoder_config.layer_drop_p = 0.1
+
+    config.use_masking = False
+    config.max_temporal_mask_prob = 0.0
+    config.max_spatial_mask_prob = 0.0
+    config.vocab_info.size = 2475
+
+    return config
+
+
+@wav2vec2_asr_arch("1b_llama_bib61")
+def _1b_llama_bib61() -> Wav2Vec2AsrConfig:
+    config = _base_10h()
+
+    config.encoder_config = wav2vec2_encoder_archs.get("1b_llama")
+    config.encoder_config.feature_gradient_scale = 1.0
+    config.encoder_config.dropout_p = 0.0
+    config.encoder_config.attn_dropout_p = 0.0
+    config.encoder_config.ffn_inner_dropout_p = 0.1
+    config.encoder_config.layer_drop_p = 0.1
 
     config.use_masking = False
     config.max_temporal_mask_prob = 0.0
@@ -125,7 +144,7 @@ def _2b_bib61() -> Wav2Vec2AsrConfig:
     config.encoder_config.dropout_p = 0.0
     config.encoder_config.attn_dropout_p = 0.0
     config.encoder_config.ffn_inner_dropout_p = 0.1
-    config.encoder_config.layer_drop_p = 0.0
+    config.encoder_config.layer_drop_p = 0.1
 
     config.use_masking = False
     config.max_temporal_mask_prob = 0.0
@@ -144,7 +163,7 @@ def _3b_bib61() -> Wav2Vec2AsrConfig:
     config.encoder_config.dropout_p = 0.0
     config.encoder_config.attn_dropout_p = 0.0
     config.encoder_config.ffn_inner_dropout_p = 0.1
-    config.encoder_config.layer_drop_p = 0.0
+    config.encoder_config.layer_drop_p = 0.1
 
     config.use_masking = False
     config.max_temporal_mask_prob = 0.0
@@ -163,7 +182,7 @@ def _3b_higher_bib61() -> Wav2Vec2AsrConfig:
     config.encoder_config.dropout_p = 0.0
     config.encoder_config.attn_dropout_p = 0.0
     config.encoder_config.ffn_inner_dropout_p = 0.1
-    config.encoder_config.layer_drop_p = 0.0
+    config.encoder_config.layer_drop_p = 0.1
 
     config.use_masking = False
     config.max_temporal_mask_prob = 0.0
