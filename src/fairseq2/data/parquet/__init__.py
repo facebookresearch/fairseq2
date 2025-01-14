8cc193d5249d4b28c0a81926f0064a544bf930ed
yaoj <yaoj@meta.com>
2025-01-14 15:23:57 +0000

    add draft pipeline

--- src/fairseq2/data/parquet/__init__.py
+++ src/fairseq2/data/parquet/__init__.py
@@ -1,24 +1,52 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from fairseq2.data.parquet.configs import (
     DataLoadingConfig,
     EvaluationDataLoadingConfig,
     ParquetBatchFormat,
     ParquetDatasetLimitOptions,
     ValidationDataLoadingConfig,
+    ParquetBasicDataloaderConfig,
+)
+from fairseq2.data.parquet.pipeline import (
+    init_parquet_dataset,
+    list_parquet_fragments,
+    build_iterator_over_one_table,
+)
+from fairseq2.data.parquet.transform import (
+    affix_list_column,
+    apply_filter,
+    build_uniform_list_column,
+    concat_table,
+    filter_list_by_range,
+    filter_rows_by_consistent_list_length,
+    filter_strings_by_length,
+    maybe_cast,
+    replace_table_column,
 )
 from fairseq2.data.parquet.utils import (
+    _TableWrapper,
+    _to_real_object,
     BatchOutputType,
     NestedDict,
     NestedDictValue,
     add_partitioning_values,
-    apply_filter,
+    compute_length_splits,
     compute_rows_length,
-    concat_table,
-    from_pyarrow_to_torch_tensor,
     get_dataset_fragments,
-    get_parquet_dataset_metadata,
-    get_row_group_level_metadata,
+    hstack_pyarray_list,
+    load_one_fragment,
+    pyarrow_cpu,
     pyarrow_table_to_torch_dict,
+    pyarrow_to_torch_tensor,
+    pyarrow_column_to_array,
     split_fragment_in_row_groups,
+    table_func_wrap,
+    torch_random_seed,
 )
 
 __all__ = [
@@ -28,18 +56,38 @@ __all__ = [
     "ValidationDataLoadingConfig",
     "EvaluationDataLoadingConfig",
     "ParquetBatchFormat",
+    "ParquetBasicDataloaderConfig",
     # --- utils --- #
+    "_TableWrapper",
+    "_to_real_object",
+    "BatchOutputType",
     "NestedDict",
     "NestedDictValue",
     "add_partitioning_values",
+    "compute_length_splits",
     "compute_rows_length",
     "get_dataset_fragments",
+    "hstack_pyarray_list",
+    "load_one_fragment",
+    "pyarrow_cpu",
+    "pyarrow_table_to_torch_dict",
+    "pyarrow_to_torch_tensor",
+    "pyarrow_column_to_array",
     "split_fragment_in_row_groups",
-    "from_pyarrow_to_torch_tensor",
-    "BatchOutputType",
+    "table_func_wrap",
+    "torch_random_seed",
+    # --- transform --- #
     "apply_filter",
     "concat_table",
-    "get_parquet_dataset_metadata",
-    "get_row_group_level_metadata",
-    "pyarrow_table_to_torch_dict",
+    "replace_table_column",
+    "affix_list_column",
+    "build_uniform_list_column",
+    "filter_list_by_range",
+    "filter_rows_by_consistent_list_length",
+    "filter_strings_by_length",
+    "maybe_cast",
+    # --- pipeline --- #
+    "init_parquet_dataset",
+    "list_parquet_fragments",
+    "build_iterator_over_one_table",
 ]
