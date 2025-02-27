83aae217ac411203264789a61e21a52169583636
Can Balioglu <cbalioglu@users.noreply.github.com>
2025-02-27 17:29:41 -0500

    Improve FSDP wrapper implementation (#1074)

--- src/fairseq2/nn/data_parallel/__init__.py
+++ src/fairseq2/nn/data_parallel/__init__.py
@@ -10,20 +10,12 @@ from fairseq2.nn.data_parallel._ddp import to_ddp as to_ddp
 from fairseq2.nn.data_parallel._error import (
     DistributedSetupError as DistributedSetupError,
 )
+from fairseq2.nn.data_parallel._fsdp import FsdpApplier as FsdpApplier
+from fairseq2.nn.data_parallel._fsdp import FsdpGranularity as FsdpGranularity
 from fairseq2.nn.data_parallel._fsdp import (
-    FSDP_LOW_MEMORY_POLICY as FSDP_LOW_MEMORY_POLICY,
+    FsdpParameterInitializer as FsdpParameterInitializer,
 )
-from fairseq2.nn.data_parallel._fsdp import (
-    FSDP_STANDARD_MEMORY_POLICY as FSDP_STANDARD_MEMORY_POLICY,
-)
-from fairseq2.nn.data_parallel._fsdp import (
-    FSDP_VERY_LOW_MEMORY_POLICY as FSDP_VERY_LOW_MEMORY_POLICY,
-)
-from fairseq2.nn.data_parallel._fsdp import FSDPMemoryPolicy as FSDPMemoryPolicy
-from fairseq2.nn.data_parallel._fsdp import (
-    FSDPParameterInitializer as FSDPParameterInitializer,
-)
-from fairseq2.nn.data_parallel._fsdp import FSDPWrapPolicy as FSDPWrapPolicy
+from fairseq2.nn.data_parallel._fsdp import FsdpWrapper as FsdpWrapper
 from fairseq2.nn.data_parallel._fsdp import (
     fsdp_local_state_dict as fsdp_local_state_dict,
 )
