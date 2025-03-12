6ce120c83a94bdbf481d2796ac0c3af626a8d3e3
Can Balioglu <cbalioglu@users.noreply.github.com>
2025-03-12 17:59:39 -0400

    Support tied embeddings in LLaMA (#1082)

--- src/fairseq2/models/llama/_config.py
+++ src/fairseq2/models/llama/_config.py
@@ -36,6 +36,9 @@ class LLaMAConfig:
     )
     """The vocabulary information."""
 
+    tie_embeddings: bool = False
+    """If ``True``, ties the embedding table and the output projection layer."""
+
     num_layers: int = 32
     """The number of decoder layers."""
 
@@ -245,6 +248,7 @@ def register_llama_configs(context: RuntimeContext) -> None:
         config = llama3_1_8b()
 
         config.model_dim = 2048
+        config.tie_embeddings = True
         config.ffn_inner_dim = 2048 * 4
         config.ffn_inner_dim_multiplier = 1.5
         config.ffn_inner_dim_to_multiple = 256
