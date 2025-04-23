5a215b9df3329efb46754db88fe44fb32f7fac86
mattsetzler <mattsetzler@devvm6557.ash0.facebook.com>
2025-04-23 14:30:42 -0700

    Add support for writing out model confidence scores.

--- src/fairseq2/models/asr/_model.py
+++ src/fairseq2/models/asr/_model.py
@@ -8,14 +8,15 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import List
 
 import torch
-from torch import Tensor
-from torch.nn import Module
-from torch.nn.functional import ctc_loss, log_softmax
 
 from fairseq2.models.sequence import SequenceBatch
-from fairseq2.nn.padding import PaddingMask, get_seq_lens, pad_seqs
+from fairseq2.nn.padding import get_seq_lens, pad_seqs, PaddingMask
+from torch import masked, Tensor
+from torch.nn import Module
+from torch.nn.functional import ctc_loss, log_softmax
 
 
 class AsrModel(Module, ABC):
@@ -107,3 +108,22 @@ class AsrModelOutput:
 
         # (N, S), (N, S)
         return pad_seqs(hyp_seq_list, pad_value=pad_idx)
+
+    def generate_hypotheses_with_confidences(
+        self, pad_idx: int, blank_label: int = 0
+    ) -> tuple[Tensor, PaddingMask | None, List[float]]:
+        hypotheses, padding_mask = self.generate_hypotheses(pad_idx, blank_label)
+
+        if isinstance(padding_mask, PaddingMask):
+            # (N, S)
+            conf = self.logits.softmax(dim=2).max(dim=2).values
+            masked_conf = conf.masked_fill(padding_mask.materialize(), 0)
+
+            # (N)
+            mean_conf = masked_conf.sum(dim=1) / (~padding_mask.materialize()).sum(
+                dim=1
+            )
+        else:
+            mean_conf = self.logits.softmax(dim=2).max(dim=2).values.mean(dim=1)  # (N)
+
+        return hypotheses, padding_mask, mean_conf.tolist()
