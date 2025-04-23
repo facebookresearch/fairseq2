5b5ce4e627d948815e436fda8336559a1ba5ba39
Artyom Kozhevnikov <artyomko@fb.com>
2025-04-23 05:02:23 -0700

    typo

--- src/fairseq2/file_system.py
+++ src/fairseq2/file_system.py
@@ -311,7 +311,7 @@ def _register_filesystems(context: Any) -> None:
                 lambda p: str(p).startswith(prefix),
                 lambda: FSspecFileSystem(fsspec, prefix),
             )
-        except ImportError, OSError:
+        except (ImportError, OSError):
             pass
 
 
