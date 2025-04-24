d9382b13e9bc87ad5ce6cc6f9520eef9eae92827
Artyom Kozhevnikov <artyomko@fb.com>
2025-04-24 06:49:58 -0700

    flake8

--- src/fairseq2/datasets/speech_parquet.py
+++ src/fairseq2/datasets/speech_parquet.py
@@ -37,8 +37,6 @@ from fairseq2.data.parquet import (
 )
 from fairseq2.datasets import (
     DataPipelineReader,
-    LengthBatching,
-    StaticBatching,
     UnknownSplitError,
 )
 from fairseq2.datasets.speech import (
