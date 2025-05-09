9d320da6dc66fa237036dd2994f348585dd01f93
gilkeren1 <gilkeren@meta.com>
2025-05-09 16:20:43 -0700

    Add Add Llama-style ASR to MMS, the fs2 side (#1171)
    * Upleveling RNNT to new fs2
    
    * Wav2vec2Llama upleveled

--- src/fairseq2/models/wav2vec2/asr/__init__.py
+++ src/fairseq2/models/wav2vec2/asr/__init__.py
@@ -6,8 +6,24 @@
 
 from __future__ import annotations
 
+from fairseq2.models.wav2vec2.asr._factory import (
+    init_final_projection as init_final_projection,
+)
+from fairseq2.models.wav2vec2.asr._model import Wav2Vec2AsrModel as Wav2Vec2AsrModel
 from fairseq2.models.wav2vec2.asr.checkpoint import (
-    _convert_wav2vec2_asr_checkpoint as _convert_wav2vec2_asr_checkpoint,
+    ModelHubAccessor,
+)
+from fairseq2.models.wav2vec2.asr.checkpoint import (
+    _convert_wav2vec2_asr_checkpoint as _convert_wav2vec2_asr_checkpoint,  # isort: split
+)
+from fairseq2.models.wav2vec2.asr.checkpoint import (
+    convert_wav2vec2_asr_checkpoint as convert_wav2vec2_asr_checkpoint,
+)
+from fairseq2.models.wav2vec2.asr.checkpoint import (
+    fairseq2.models,
+    fairseq2.models.wav2vec2.asr._checkpoint,
+    from,
+    import,
 )
 from fairseq2.models.wav2vec2.asr.config import (
     WAV2VEC2_ASR_FAMILY as WAV2VEC2_ASR_FAMILY,
@@ -26,3 +42,5 @@ from fairseq2.models.wav2vec2.asr.hub import (
     get_wav2vec2_asr_model_hub as get_wav2vec2_asr_model_hub,
 )
 from fairseq2.models.wav2vec2.asr.model import Wav2Vec2AsrModel as Wav2Vec2AsrModel
+
+get_wav2vec2_asr_model_hub = ModelHubAccessor(Wav2Vec2AsrModel, Wav2Vec2AsrConfig)
