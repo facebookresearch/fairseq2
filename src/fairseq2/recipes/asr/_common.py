17842fa3887324c0a1d6a2a886d30968adfd6dc4
gilkeren <gilkeren@meta.com>
2025-05-27 15:42:01 -0700

    Debug commit upleveled latest no PL

--- src/fairseq2/recipes/asr/_common.py
+++ src/fairseq2/recipes/asr/_common.py
@@ -7,20 +7,23 @@
 from __future__ import annotations
 
 import math
-from typing import Any, Dict, TextIO, final
+import re
+from typing import Any, Dict, final, TextIO
 
 import torch
-from torch import Tensor
-from typing_extensions import override
 
 from fairseq2.data.text.tokenizers import TextTokenDecoder, TextTokenizer
 from fairseq2.gang import Gang
+
+from fairseq2.logging import log
 from fairseq2.metrics import Mean
 from fairseq2.metrics.text import WerMetric
 from fairseq2.models.asr import AsrModel, AsrModelOutput
 from fairseq2.models.seq2seq import Seq2SeqBatch
 from fairseq2.models.sequence import SequenceBatch
 from fairseq2.recipes import BaseMetricBag, Model, UnitError
+from torch import Tensor
+from typing_extensions import override
 
 
 @final
@@ -41,8 +44,10 @@ class AsrCriterion:
     def __call__(
         self, batch: Seq2SeqBatch, metric_bag: AsrMetricBag
     ) -> tuple[Tensor, int]:
+        log.info(f"s3: calling forward")
         output = self._forward(batch)
 
+        log.info(f"s4: calling loss")
         loss, extra_metrics = output.compute_loss(
             batch.target_seqs, batch.target_padding_mask
         )
@@ -53,8 +58,10 @@ class AsrCriterion:
 
         metric_bag.update_extra_metrics(batch, extra_metrics)
 
+        log.info(f"s5: calling scorer")
         if self._scorer is not None:
             self._scorer(batch, output, metric_bag)
+        log.info(f"s6: done scorer")
 
         return loss, batch.batch_size
 
@@ -116,6 +123,11 @@ class AsrScorer:
 
         refs = [self._text_decoder(s) for s in ref_seqs]
         hyps = [self._text_decoder(s) for s in hyp_seqs]
+
+        for r, h in zip(refs, hyps):
+            if torch.rand([]) < 0.01 or bool(re.search(r"[\u0590-\u05FF]", r)):
+                log.info(f"Reference: {r}")
+                log.info(f"Hypothesis: {h}")
 
         metric_bag.wer.update(
             refs, ref_seqs, ref_padding_mask, hyps, hyp_seqs, hyp_padding_mask
--- src/fairseq2/recipes/wav2vec2/asr/_train.py
+++ src/fairseq2/recipes/wav2vec2/asr/_train.py
@@ -6,17 +6,17 @@
 
 from __future__ import annotations
 
+import socket
+
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Literal, cast, final
+from typing import cast, final, Literal
 
 import torch
-from torch import Tensor
-from typing_extensions import override
 
 from fairseq2.context import RuntimeContext
 from fairseq2.datasets import LengthBatching, SyncMode
-from fairseq2.datasets.asr import GENERIC_ASR_DATASET_FAMILY, AsrDataset
+from fairseq2.datasets.asr import AsrDataset, GENERIC_ASR_DATASET_FAMILY
 from fairseq2.datasets.speech import SpeechReadOptions
 from fairseq2.gang import Gang, GangError
 from fairseq2.logging import log
@@ -60,13 +60,15 @@ from fairseq2.recipes.config import (
 )
 from fairseq2.recipes.utils.log import log_model
 from fairseq2.recipes.wav2vec2.batch_weighted_datareader import (
-    MIXTURE_DATASET_FAMILY,
     BatchMixtureDataset,
+    MIXTURE_DATASET_FAMILY,
 )
 from fairseq2.typing import CPU
 from fairseq2.utils.rng import manual_seed
 from fairseq2.utils.structured import structure
 from fairseq2.utils.validation import validate
+from torch import Tensor
+from typing_extensions import override
 
 
 @dataclass(kw_only=True)
@@ -122,6 +124,7 @@ class Wav2Vec2AsrTrainConfig:
             validate_after_n_steps=10_000,
             validate_every_n_steps=1_000,
             publish_metrics_every_n_steps=200,
+            keep_last_n_checkpoints=1,
         )
     )
 
@@ -263,7 +266,12 @@ def load_wav2vec2_asr_trainer(
 
     # If we start the training with an empty ASR model, use the weights of a
     # pretrained wav2vec 2.0 model.
-    if model.is_empty_initialized:
+    machine_name = socket.gethostname()
+    if (
+        model.is_empty_initialized
+        and config.pretrained_model.name
+        and not machine_name.startswith("devvm")
+    ):
         pt_model = load_reference_model(
             Wav2Vec2Model,
             context,
