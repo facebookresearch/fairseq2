0d3d85229e32f8d21cf27d80483acc60f2f852ba
Alexander Erben <aerben@meta.com>
2025-07-11 12:04:44 -0400

    configured cudnn to be deterministic

--- src/fairseq2/recipes/common/_torch.py
+++ src/fairseq2/recipes/common/_torch.py
@@ -107,6 +107,9 @@ def _set_numerics(torch_section: TorchSection) -> None:
     if not torch.cuda.is_available():
         return
 
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     # Matrix Multiplications
     torch.backends.cuda.matmul.allow_tf32 = torch_section.allow_tf32
 
