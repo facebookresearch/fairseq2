7e98fa334965ea085bb5d66aff6c628184a3873a
mattsetzler <mattsetzler@devvm3964.rva0.facebook.com>
2025-08-06 13:01:28 -0700

    CTC LM Fusion.

--- src/fairseq2/models/asr/_model.py
+++ src/fairseq2/models/asr/_model.py
@@ -8,16 +8,21 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
+import kenlm
 import torch
-from torch import Tensor
-from torch.nn import Module
-from torch.nn.functional import ctc_loss, log_softmax
+
+import torch.nn.functional as F
+from fairseq2.data.text.tokenizers import TextTokenizer
 
 from fairseq2.models.seq2seq import Seq2SeqBatch
 from fairseq2.models.sequence import SequenceBatch
-from fairseq2.nn.padding import PaddingMask, get_seq_lens, pad_seqs
+from fairseq2.nn.padding import get_seq_lens, pad_seqs, PaddingMask
+
+from torch import Tensor
+from torch.nn import Module
+from torch.nn.functional import ctc_loss, log_softmax
 
 
 class AsrModel(Module, ABC):
@@ -28,11 +33,42 @@ class AsrModel(Module, ABC):
 
 
 @dataclass
+class CTCBeam:
+    hyp: List[int]
+    score: float = 0.0
+
+
+def _get_ngram_score(
+    ngram_model: kenlm.Model,
+    tokenizer: TextTokenizer,
+    parent_hyp: List[int],
+    char_idx: int,
+    blank_label: int = 0,
+) -> float:
+    assert char_idx != blank_label
+
+    print("rescoring...")
+    decoder = tokenizer.create_decoder()
+    toks = torch.tensor(parent_hyp).unique_consecutive()
+    toks = toks[toks != blank_label]
+
+    hyp_parent = " ".join(decoder(torch.tensor(parent_hyp)))
+    new_char = decoder(torch.tensor([char_idx]))
+    hyp = hyp_parent + " " + new_char
+
+    ngram_conditional_prob = ngram_model.score(
+        hyp, bos=True, eos=False
+    ) - ngram_model.score(hyp_parent, bos=True, eos=False)
+    print("done rescoring!")
+    return ngram_conditional_prob
+
+
+@dataclass
 class AsrModelOutput:
     logits: Tensor
-    """The logits for next-step prediction. *Shape:* :math:`(N,S_{out},T)`,
+    """The logits for next-step prediction. *Shape:* :math:`(N,S_{out},V)`,
     where :math:`N` is the batch size, :math:`S_{out}` is the output sequence
-    length, and :math:`T` is the size of the vocabulary."""
+    length, and :math:`V` is the size of the vocabulary."""
 
     padding_mask: PaddingMask | None
     """The padding mask of :attr:`logits`. *Shape:* :math:`(N,S_{out})`, where
@@ -54,10 +90,10 @@ class AsrModelOutput:
             A scalar tensor representing the summed CTC loss.
         """
         # For numerical stability run in single precision.
-        # (N, S, T)
+        # (N, S, V)
         lprobs = log_softmax(self.logits, dim=-1, dtype=torch.float32)
 
-        # (N, S, T) -> (S, N, T)
+        # (N, S, V) -> (S, N, V)
         lprobs_t = lprobs.transpose(0, 1)
 
         # (N)
@@ -78,6 +114,93 @@ class AsrModelOutput:
             ),
             {},
         )
+
+    @torch.no_grad()
+    def generate_hypotheses_ngram_fuse_single_row(
+        self,
+        pad_idx: int,
+        seq_len: int,
+        log_probs: Tensor,  # [S, V]
+        ngram_model: kenlm.Model,
+        tokenizer: TextTokenizer,
+        blank_label: int = 0,
+        nbest: int = 5,  # n beams
+        topk: int = 100,  # only rescore top-k CTC candidates per beam
+        rescore_beta: float = 0.1,
+    ) -> Tensor:
+        beams = [CTCBeam(hyp=[], score=0.0)] * nbest
+        _, V = log_probs.size()
+
+        for t in range(seq_len):
+            new_beams = []
+            _, cand_idxs_t = log_probs[t].topk(k=topk, dim=-1)
+
+            for beam in beams:
+                for v in range(V):
+                    prfx = beam.hyp
+                    if len(prfx) == 0:
+                        seq = [v]
+                    else:
+                        seq = beam.hyp.append(v)
+                    if v == blank_label or v == beam.hyp[-1]:
+                        score = beam.score + log_probs[t, v]
+                    elif v not in cand_idxs_t:
+                        score = -torch.inf  # only rescore top-K candidates
+                    else:
+                        score = (
+                            beam.score
+                            + log_probs[t, v]
+                            + rescore_beta
+                            * _get_ngram_score(
+                                ngram_model, tokenizer, beam.hyp, v, blank_label
+                            )
+                        )
+                    # assert isinstance(seq, list)
+                    # assert isinstance(score, float)
+                    new_beams.append(CTCBeam(hyp=seq, score=score))  # type: ignore
+
+            new_beams = sorted(new_beams, key=lambda x: x.score, reverse=True)[:nbest]
+            beams = new_beams
+
+        best_beam = beams[0]
+        best_seq = torch.tensor(best_beam.hyp)
+        best_seq = best_seq.unique_consecutive()
+        best_seq = best_seq[best_seq != blank_label]
+        return best_seq
+
+    @torch.no_grad()
+    def generate_hypotheses_ngram_fuse(
+        self,
+        pad_idx: int,
+        tokenizer: TextTokenizer,
+        ngram_model_dict: Dict[str, kenlm.Model],
+        languages: List[str],  # list of languages [N]
+        blank_label: int = 0,
+        nbest: int = 5,
+        rescore_beta: float = 0.1,
+    ) -> tuple[Tensor, PaddingMask | None]:
+
+        seq_lens = get_seq_lens(self.logits, self.padding_mask)
+        log_probs = F.log_softmax(self.logits, dim=-1, dtype=torch.float32)  # (N, S, V)
+
+        hyp_seq_list = []
+        for language, log_prob, seq_len in zip(languages, log_probs, seq_lens):
+            print("generating hypothesis using ngram fusion...")
+            hyp_seq = self.generate_hypotheses_ngram_fuse_single_row(
+                pad_idx,
+                int(seq_len.item()),
+                log_prob,
+                ngram_model_dict[language],
+                tokenizer,
+                blank_label,
+                nbest,
+                rescore_beta=rescore_beta,
+            )
+            hyp_seq_list.append(hyp_seq)
+            print("done generating hypothesis!")
+
+        # (N, S), (N, S)
+        return pad_seqs(hyp_seq_list, pad_value=pad_idx)
 
     def generate_hypotheses(
         self, pad_idx: int, blank_label: int = 0
