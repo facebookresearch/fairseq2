d8b1464345ce7834c13fd3cee357882e0adbf962
Can Balioglu <cbalioglu@users.noreply.github.com>
2025-09-14 16:19:58 +0000

    Introduce support for optimizer parameter groups

--- src/fairseq2/recipe/config.py
+++ src/fairseq2/recipe/config.py
@@ -56,6 +56,14 @@ class SupportsStructure(ABC):
     def structure(self, resolver: DependencyResolver) -> None: ...
 
 
+Default: TypeAlias = Literal["default"]
+"""
+Indicates that the default value of a particular configuration field, as defined
+in a higher-level configuration, should be used. See :class:`AdamWGroupConfig`
+as an example.
+"""
+
+
 @dataclass(kw_only=True)
 class ModelSection(Validatable):
     name: str | None = None
@@ -713,12 +721,16 @@ class OptimizerSection(SupportsStructure):
 
 @dataclass(kw_only=True)
 class AdamWConfig:
+    """Represents the configuration of :class:`AdamW`."""
+
     lr: float = 1e-3
     """The learning rate."""
 
     betas: tuple[float, float] = (0.9, 0.999)
-    """The coefficients used for computing running averages of gradient and its
-    square."""
+    """
+    The coefficients used for computing running averages of gradient and its
+    square.
+    """
 
     eps: float = 1e-8
     """The term added to the denominator to improve numerical stability."""
@@ -741,10 +753,45 @@ class AdamWConfig:
     impl: Literal["auto", "foreach", "fused", "naive"] = "auto"
     """The implementation variant. See :class:`torch.optim.AdamW` for details."""
 
+    groups: Sequence[AdamWGroupConfig] = field(default_factory=list)
+    """
+    The configuration of individual parameter groups. Each parameter will be
+    assigned to the first group in the list that matches its name. Parameters
+    that do not match any group will use the top-level configuration.
+    """
+
+
+@dataclass(kw_only=True)
+class AdamWGroupConfig:
+    """Represents the configuration of an :class:`AdamW` parameter group."""
+
+    params: str | Sequence[str] = ".*"
+    """
+    The regular expression(s) used to select the parameters that belong to this
+    group.
+    """
+
+    lr: float | Default = "default"
+    """The learning rate."""
+
+    betas: tuple[float, float] | Default = "default"
+    """
+    The coefficients used for computing running averages of gradient and its
+    square.
+    """
+
+    eps: float | Default = "default"
+    """The term added to the denominator to improve numerical stability."""
+
+    weight_decay: float | Default = "default"
+    """The weight decay coefficient."""
+
 
 @dataclass(kw_only=True)
 class AdafactorConfig:
     """
+    Represents the configuration of :class:`Adafactor`.
+
     Adafactor is an optimizer that saves memory compared to AdamW by using
     low-rank representation of gradient running averages. It is recommended to
     use higher learning rate with it.
@@ -755,8 +802,8 @@ class AdafactorConfig:
 
     beta2_decay: float = -0.8
     """
-    The decay rate of beta2. beta2 standardly refers to the coefficient used for
-    computing the running average of the gradient squared.
+    The decay rate of beta2. beta2 refers to the coefficient used for computing
+    the running average of the gradient squared.
     """
 
     eps: tuple[float | None, float] = (None, 0.001)
@@ -773,10 +820,50 @@ class AdafactorConfig:
     """The weight decay coefficient."""
 
     foreach: bool | None = None
-    """Whether ``foreach`` implementation is used."""
+    """If ``True``, ``foreach`` implementation is used."""
 
     maximize: bool = False
     """If ``True``, maximizes the parameters instead of minimizing."""
+
+    groups: Sequence[AdafactorGroupConfig] = field(default_factory=list)
+    """
+    The configuration of individual parameter groups. Each parameter will be
+    assigned to the first group in the list that matches its name. Parameters
+    that do not match any group will use the top-level configuration.
+    """
+
+
+@dataclass(kw_only=True)
+class AdafactorGroupConfig:
+    """Represents the configuration of an :class:`Adafactor` parameter group."""
+
+    params: str | Sequence[str] = ".*"
+    """
+    The regular expression(s) used to select the parameters that belong to this
+    group.
+    """
+
+    lr: float | Default = "default"
+    """The learning rate."""
+
+    beta2_decay: float | Default = "default"
+    """
+    The decay rate of beta2. beta2 standardly refers to the coefficient used for
+    computing the running average of the gradient squared.
+    """
+
+    eps: tuple[float | None, float] | Default = "default"
+    """
+    epsilon1 is the term added to the denominator of the update calculation to
+    improve numerical stability. epsilon2 is the term used to avoid having too
+    small a weight update when applying parameter scaling.
+    """
+
+    d: float | Default = "default"
+    """The clipping threshold, used to avoid larger-than-desired updates."""
+
+    weight_decay: float | Default = "default"
+    """The weight decay coefficient."""
 
 
 PASSTHROUGH_LR: Final = "passthrough"
