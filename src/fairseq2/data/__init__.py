0cfdf83ce05ec7e9fee408b55570ebfe28996f82
jacklanchantin <jacklanchantin@gmail.com>
2025-09-24 14:37:12 +0000

    isort

--- src/fairseq2/data/__init__.py
+++ src/fairseq2/data/__init__.py
@@ -1,9 +1,7 @@
-from fairseq2.data.data_pipeline import create_bucket_sizes as create_bucket_sizes
+from fairseq2.data.data_pipeline import CollateOptionsOverride as CollateOptionsOverride
 from fairseq2.data.data_pipeline import Collater as Collater
-from fairseq2.data.data_pipeline import SequenceData as SequenceData
 from fairseq2.data.data_pipeline import DataPipelineBuilder as DataPipelineBuilder
-from fairseq2.data.data_pipeline import (
-    CollateOptionsOverride as CollateOptionsOverride,
-)
+from fairseq2.data.data_pipeline import SequenceData as SequenceData
+from fairseq2.data.data_pipeline import create_bucket_sizes as create_bucket_sizes
 
 __all__: list[str] = []
