b36dffe6ed6207c36257b3afc074476787f2cf56
Jack Lanchantin <jacklanchantin@gmail.com>
2025-10-22 23:56:39 -0400

    allow batch_sz > 1

--- src/fairseq2/recipes/lm/_online_finetune/_grpo.py
+++ src/fairseq2/recipes/lm/_online_finetune/_grpo.py
@@ -301,7 +301,7 @@ class GrpoFinetuneUnit(TrainUnit[SequenceBatch]):
         )
 
         model_logps = self._gather_lprobs(grpo_model_logits, grpo_target_batch)
-        vllm_logps = get_vllm_logprobs(rollouts[0], self._gangs).to(model_logps.device)
+        vllm_logps = get_vllm_logprobs(rollouts, self._gangs).to(model_logps.device)
 
         tgt_logit_entropy = compute_token_level_entropy(
             grpo_model_logits, grpo_target_batch.target_mask
@@ -388,6 +388,7 @@ class GrpoFinetuneUnit(TrainUnit[SequenceBatch]):
         batch_size = advantages.size(0)
         num_rollouts = advantages.size(1)
         model_logps = model_logps.view(batch_size, num_rollouts, -1)
+        vllm_logps = vllm_logps.view(batch_size, num_rollouts, -1)
 
         per_token_scaled_advantage = (
             model_logps - model_logps.detach()
