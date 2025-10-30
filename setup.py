591f4d3a41f399b63932d9f9e393f98c09a9f81b
Lucca Bertoncini <32229669+luccabb@users.noreply.github.com>
2025-10-30 03:59:01 -0700

    Move cluster discovery to clusterscope (#1326)

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     zip_safe=False,
     python_requires=">=3.10",
     install_requires=[
+        "clusterscope~=0.0.31",
         "editdistance~=0.8",
         "fairseq2n" + fairseq2n_version_spec,
         "huggingface_hub~=0.32",
