7a1f1d0b4003a5c6729d43748363dc2a480a4a99
Can Balioglu <cbalioglu@users.noreply.github.com>
2025-11-03 19:43:33 +0000

    Improve the implementation of Uri

--- src/fairseq2/utils/uri.py
+++ src/fairseq2/utils/uri.py
@@ -6,26 +6,77 @@
 
 from __future__ import annotations
 
+from collections.abc import Mapping
 from pathlib import Path
 from typing import final
-from urllib.parse import ParseResult, urlparse, urlunparse
+from urllib.parse import ParseResult, unquote, urlparse, urlunparse
 
-from fairseq2.error import InternalError
+from fairseq2.error import InternalError, NotSupportedError
 
 
 @final
 class Uri:
+    """Represents a Uniform Resource Identifier."""
+
+    @classmethod
+    def parse(cls, s: str) -> Uri:
+        try:
+            result = urlparse(s)
+        except ValueError as ex:
+            raise UriFormatError(f"{s} cannot be parsed as URI.") from ex
+
+        if not result.scheme:
+            raise UriFormatError(f"{s} does not have a URI scheme.")
+
+        params = cls._parse_params(s, result.params)
+
+        return Uri(result, params)
+
+    @staticmethod
+    def _parse_params(s: str, params: str) -> dict[str, str]:
+        output: dict[str, str] = {}
+
+        params = params.strip()
+        if not params:
+            return output
+
+        def unquote_and_strip(idx: int, p: str) -> str:
+            try:
+                p = unquote(p)
+            except (UnicodeEncodeError, ValueError) as ex:
+                raise UriFormatError(
+                    f"Path parameters of {s} are expected to be valid quoted URI strings, but parameter at index {idx} cannot be unquoted."
+                ) from ex
+
+            return p.strip()
+
+        pairs = params.split(";")
+
+        for idx, param in enumerate(pairs):
+            kv = param.split("=")
+            if len(kv) != 2:
+                raise UriFormatError(
+                    f"Path parameters of {s} are expected to be semi-colon separated key-value pairs, but parameter at index {idx} is {param}."
+                )
+
+            key, value = kv
+
+            key = unquote_and_strip(idx, key)
+            if not key:
+                raise UriFormatError(
+                    f"Path parameter keys of {s} are expected to be non-empty, but parameter key at index {idx} is empty."
+                )
+
+            output[key] = unquote_and_strip(idx, value)
+
+        return output
+
     @staticmethod
     def maybe_parse(s: str) -> Uri | None:
         try:
-            result = urlparse(s)
-        except ValueError:
+            return Uri.parse(s)
+        except UriFormatError:
             return None
-
-        if not result.scheme:
-            return None
-
-        return Uri(result)
 
     @staticmethod
     def from_path(path: Path) -> Uri:
@@ -37,8 +88,9 @@ class Uri:
 
         return uri
 
-    def __init__(self, _result: ParseResult) -> None:
+    def __init__(self, _result: ParseResult, _params: dict[str, str]) -> None:
         self._result = _result
+        self._params = _params
 
     @property
     def scheme(self) -> str:
@@ -57,6 +109,10 @@ class Uri:
         return self._result.params
 
     @property
+    def parsed_params(self) -> Mapping[str, str]:
+        return self._params
+
+    @property
     def query(self) -> str:
         return self._result.query
 
@@ -64,5 +120,25 @@ class Uri:
     def fragment(self) -> str:
         return self._result.fragment
 
+    def strip_params(self) -> Uri:
+        """Returns a copy of this URI with path parameters removed."""
+        result = self._result._replace(params="")
+
+        return Uri(result, {})
+
+    def to_path(self) -> Path:
+        if self.scheme != "file":
+            raise NotSupportedError(
+                "`to_path()` is only supported for URIs with file scheme."
+            )
+
+        s = str(self)
+
+        return Path(s[5:])
+
     def __str__(self) -> str:
         return urlunparse(self._result)
+
+
+class UriFormatError(Exception):
+    pass
