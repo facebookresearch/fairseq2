0f05b96c35d5305c563087df9834537f4ddbee1c
Can Balioglu <cbalioglu@users.noreply.github.com>
2025-11-06 18:17:16 -0500

    Bump to v0.8.0.dev0 (#1409)

--- native/python/setup.py
+++ native/python/setup.py
@@ -136,7 +136,7 @@ setup(
         "install_cmake": install_cmake,
     },
     name="fairseq2n",
-    version="0.7.0",
+    version="0.8.0.dev0",
     description="FAIR Sequence Modeling Toolkit (Native)",
     long_description="https://github.com/facebookresearch/fairseq2",
     long_description_content_type="text/plain",
--- native/python/src/fairseq2n/__init__.py
+++ native/python/src/fairseq2n/__init__.py
@@ -6,7 +6,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.7.0"
+__version__ = "0.8.0.dev0"
 
 import platform
 import site
