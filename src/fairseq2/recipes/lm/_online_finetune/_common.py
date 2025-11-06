b38926950dd0859c8f3b2efbe6bedaa0b4d5a1dd
lidli <lidli@meta.com>
2025-11-06 23:37:57 +0000

    ppl/logp reward

--- src/fairseq2/recipes/lm/_online_finetune/_common.py
+++ src/fairseq2/recipes/lm/_online_finetune/_common.py
@@ -116,6 +116,7 @@ def generate_rollouts(
     dp_gang,
     vllm_model,
     sampling_params=None,
+    string_input=False,
 ):
     prompts_to_generate = [None] * dp_gang.size
     if dp_gang.rank == 0:
@@ -129,7 +130,9 @@ def generate_rollouts(
             flat_request_list.extend(rank_prompts)
 
         rollouts = vllm_model.rollout_from_model(
-            flat_request_list, sampling_params=sampling_params
+            prompt_list=flat_request_list,
+            sampling_params=sampling_params,
+            string_input=string_input,
         )
 
         rollouts_to_scatter = []
