6d6a57de4b70104d7e6a81b00e99c8663e55526e
Richard Yue <richardyue@meta.com>
2025-11-20 15:36:47 -0800

    C++ Syntax fixes

--- native/python/src/fairseq2n/bindings/type_casters/torch.cc
+++ native/python/src/fairseq2n/bindings/type_casters/torch.cc
@@ -18,9 +18,9 @@ using namespace fairseq2n::detail;
 struct THPVariable {
     PyObject_HEAD
     #if TORCH_VERSION_MAJOR < 2 || (TORCH_VERSION_MAJOR == 2 && TORCH_VERSION_MINOR < 10)
-    at::Tensor cdata;
-    #else
     const at::MaybeOwned<at::Tensor> cdata;
+    #else
+    at::Tensor cdata;
     #endif
 };
 
@@ -69,9 +69,9 @@ type_caster<at::Tensor>::load(handle src, bool)
 
     if (isinstance<at::Tensor>(ptr)) {
         #if TORCH_VERSION_MAJOR < 2 || (TORCH_VERSION_MAJOR == 2 && TORCH_VERSION_MINOR < 10)
-        value = reinterpret_cast<THPVariable *>(ptr)->cdata;
-        #else
         value = const *reinterpret_cast<THPVariable *>(ptr)->cdata;
+        #else
+	value = reinterpret_cast<THPVariable *>(ptr)->cdata;
         #endif
         return true;
     }
