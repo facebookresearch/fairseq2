722bdbdc4ad10b56e83a6d01eb44e3edcc2571fb
Alexander Erben <aerben@meta.com>
2026-03-16 15:51:43 +0000

    Add backward compat shims for recipe.validator and recipe.task (#1417)

---
+++ src/fairseq2/recipe/validator.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from fairseq2.validator import NOOP_VALIDATOR as NOOP_VALIDATOR  # noqa: F401
+from fairseq2.validator import StandardValidator as StandardValidator  # noqa: F401
+from fairseq2.validator import Validator as Validator  # noqa: F401
