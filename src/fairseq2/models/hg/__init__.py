801e77d2e2211419a10670928e05aa4557bd09f5
Richard Yue <44269389+rsyue@users.noreply.github.com>
2026-03-18 15:27:54 -0700

    Update src/fairseq2/models/hg/__init__.py
    Co-authored-by: Alexander Erben <aerben@meta.com>

--- src/fairseq2/models/hg/__init__.py
+++ src/fairseq2/models/hg/__init__.py
@@ -21,7 +21,6 @@ from fairseq2.models.hg.config import HG_FAMILY as HG_FAMILY
 from fairseq2.models.hg.config import HuggingFaceModelConfig as HuggingFaceModelConfig
 from fairseq2.models.hg.config import register_hg_configs as register_hg_configs
 
-# Re-export converter API (formerly in fairseq2/models/hg.py)
 from fairseq2.models.hg.converter import HuggingFaceConfig as HuggingFaceConfig
 from fairseq2.models.hg.converter import HuggingFaceConverter as HuggingFaceConverter
 from fairseq2.models.hg.converter import (
